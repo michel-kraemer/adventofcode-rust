15e1fb1d77a0d280893bd54d0d7300d9c27e6582
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-12-09 06:35:08 +0100

    Day 09

---
+++ day09b/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+fn recurse(numbers: &[i32]) -> i32 {
+    let nn = numbers.windows(2).map(|p| p[1] - p[0]).collect::<Vec<i32>>();
+    if !nn.iter().all(|n| *n == 0) {
+        *nn.first().unwrap() - recurse(&nn)
+    } else {
+        0
+    }
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let lines = input.lines();
+
+    let mut sum = 0;
+    for l in lines {
+        let parts = l.split(" ").map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>();
+        sum += *parts.first().unwrap() - recurse(&parts);
+    }
+
+    println!("{}", sum);
+}
