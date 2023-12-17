c8899591809dd8f07184fe8f3b90366525d05f65
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-12-17 20:41:43 +0100

    Simplify day 2

--- day02b/src/main.rs
+++ day02b/src/main.rs
@@ -1,45 +1,53 @@
-use std::{fs::File, io::{BufReader, BufRead}};
-
-use pest::Parser;
-use pest_derive::Parser;
-
-#[derive(Parser)]
-#[grammar = "game.pest"]
-struct GameParser;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 fn main() {
     let f = File::open("input.txt").expect("Could not open file");
     let reader = BufReader::new(f);
+
     let mut sum = 0;
     for line in reader.lines() {
         let line = line.expect("Could not read line");
-        let result = GameParser::parse(Rule::game, &line).unwrap().next().unwrap();
-        let inner_result = result.into_inner();
+
+        let (game, sets) = line.split_once(": ").unwrap();
+        let game: usize = game.split_once(" ").unwrap().1.parse().unwrap();
+        let sets: Vec<Vec<(usize, &str)>> = sets
+            .split("; ")
+            .map(|s| {
+                s.split(", ")
+                    .map(|c| {
+                        let p = c.split_once(" ").unwrap();
+                        (p.0.parse().unwrap(), p.1)
+                    })
+                    .collect()
+            })
+            .collect();
 
         let mut min_red = 0;
         let mut min_green = 0;
         let mut min_blue = 0;
 
-        for set in inner_result {
-            for cubes in set.into_inner() {
-                let mut inner_cubes = cubes.into_inner();
-                let number_of_cubes = inner_cubes.next().unwrap().as_str().parse::<u32>().unwrap();
-                match inner_cubes.next().unwrap().as_rule() {
-                    Rule::red => {
+        for set in sets {
+            for cubes in set {
+                let number_of_cubes = cubes.0;
+                match cubes.1 {
+                    "red" => {
                         if number_of_cubes > min_red {
                             min_red = number_of_cubes;
                         }
-                    },
-                    Rule::green => {
+                    }
+                    "green" => {
                         if number_of_cubes > min_green {
                             min_green = number_of_cubes;
                         }
-                    },
-                    Rule::blue => {
+                    }
+                    "blue" => {
                         if number_of_cubes > min_blue {
                             min_blue = number_of_cubes;
                         }
-                    },
+                    }
                     _ => panic!("Unexpected color"),
                 }
             }
@@ -48,5 +56,5 @@ fn main() {
         let power = min_red * min_green * min_blue;
         sum += power;
     }
-    println!("Result: {}", sum);
+    println!("{sum}");
 }
