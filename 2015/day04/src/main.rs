a67dab7909ceb149e9033391103f2c2511b17813
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-12-21 22:27:32 +0100

    2015: Day 4

---
+++ 2015/day04/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    for part1 in [true, false] {
+        let input = "iwrupvqb";
+
+        let mut i = 0;
+        loop {
+            let str = format!("{}{}", input, i);
+            let digest = md5::compute(str.as_bytes());
+            let digest = format!("{:x}", digest);
+            if (part1 && digest.starts_with("00000")) || (!part1 && digest.starts_with("000000")) {
+                break;
+            }
+            i += 1;
+        }
+
+        println!("{}", i);
+    }
+}
