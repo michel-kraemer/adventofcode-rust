47a638ded8cf83a4fec612890df2c1ff9cb12024
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-12-25 15:27:16 +0100

    Optimize day 25

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -11,28 +11,24 @@ use std::{
 ///
 /// See: Ulrik Brandes (2001) A Faster Algorithm for Betweenness Centrality, The
 /// Journal of Mathematical Sociology, 25:2, 163-177, DOI: 10.1080/0022250X.2001.9990249
-fn brandes<'a>(
-    vertices: &Vec<&'a str>,
-    connections: &HashMap<&'a str, HashSet<&'a str>>,
+fn brandes(
+    vertices: usize,
+    connections: &Vec<HashSet<usize>>,
     top_n: usize,
-) -> Vec<&'a str> {
-    let empty_vec = vec![];
-    let mut cb: HashMap<&str, f64> = HashMap::new();
+) -> Vec<usize> {
+    let mut cb = vec![0.; vertices];
 
-    for s in vertices {
-        let mut stack: VecDeque<&str> = VecDeque::new();
-        let mut p: HashMap<&str, Vec<&str>> = HashMap::new();
+    for s in 0..vertices {
+        let mut stack = VecDeque::new();
+        let mut p = vec![Vec::new(); vertices];
 
-        let mut rho: HashMap<&str, i64> = HashMap::new();
-        for t in vertices {
-            rho.insert(t, 0);
-        }
-        rho.insert(s, 1);
+        let mut rho = vec![0; vertices];
+        rho[s] = 1;
 
-        let mut d: HashMap<&str, i64> = HashMap::new();
-        d.insert(s, 0);
+        let mut d = vec![-1; vertices];
+        d[s] = 0;
 
-        let mut queue: VecDeque<&str> = VecDeque::new();
+        let mut queue = VecDeque::new();
         queue.push_back(s);
         while !queue.is_empty() {
             let v = queue.pop_front().unwrap();
@@ -40,93 +36,89 @@ fn brandes<'a>(
 
             for w in &connections[v] {
                 // w found for the first time?
-                if !d.contains_key(w) {
-                    queue.push_back(w);
-                    d.insert(w, d[v] + 1);
+                if d[*w] < 0 {
+                    queue.push_back(*w);
+                    d[*w] = d[v] + 1;
                 }
 
                 // shortest path to w via v?
-                if d[w] == d[v] + 1 {
-                    *rho.entry(w).or_default() += *rho.get(v).unwrap();
-                    p.entry(w).or_default().push(v);
+                if d[*w] == d[v] + 1 {
+                    rho[*w] += rho[v];
+                    p[*w].push(v);
                 }
             }
         }
 
-        let mut delta: HashMap<&str, f64> = HashMap::new();
-        for t in vertices {
-            delta.insert(t, 0.);
-        }
+        let mut delta = vec![0.; vertices];
 
         // stack returns vertices in order of non-increasing distance from s
         while !stack.is_empty() {
             let w = stack.pop_back().unwrap();
-            let pl = if p.contains_key(w) {
-                p.get(w).unwrap()
-            } else {
-                &empty_vec
-            };
-            for v in pl {
-                *delta.entry(v).or_default() += rho[v] as f64 / rho[w] as f64 * (1. + delta[w]);
-                if w != *s {
-                    *cb.entry(w).or_default() += delta[w];
+            for v in &p[w] {
+                delta[*v] += rho[*v] as f64 / rho[w] as f64 * (1. + delta[w]);
+                if w != s {
+                    cb[w] += delta[w];
                 }
             }
         }
     }
 
-    let mut cb = cb.into_iter().collect::<Vec<_>>();
+    let cb = cb.into_iter().collect::<Vec<_>>();
+    let mut cb = cb.into_iter().enumerate().collect::<Vec<_>>();
     cb.sort_by(|a, b| a.1.total_cmp(&b.1));
     cb.iter().rev().take(top_n).map(|n| n.0).collect::<Vec<_>>()
 }
 
 /// Perform DFS starting with node n and collect all found nodes until no
 /// more node can be visited
-fn dfs<'a>(
-    n: &'a str,
-    connections: &HashMap<&'a str, HashSet<&'a str>>,
-    visited: &mut HashSet<&'a str>,
+fn dfs(
+    n: usize,
+    connections: &Vec<HashSet<usize>>,
+    visited: &mut HashSet<usize>,
 ) {
-    if visited.contains(n) {
+    if visited.contains(&n) {
         return;
     }
 
     visited.insert(n);
     for c in &connections[n] {
-        dfs(c, connections, visited)
+        dfs(*c, connections, visited);
     }
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let mut vertices = HashMap::new();
     let lines = input
         .lines()
         .map(|l| {
             let a = l.split_once(": ").unwrap();
-            (a.0, a.1.split(" ").collect::<Vec<_>>())
+            let len = vertices.len();
+            let a0i = *vertices.entry(a.0).or_insert(len);
+            (a0i, a.1.split(" ").map(|v| {
+                let len = vertices.len();
+                *vertices.entry(v).or_insert(len)
+            }).collect::<Vec<_>>())
         })
         .collect::<Vec<_>>();
 
     // create a map of all bidirectional connections
-    let mut connections: HashMap<&str, HashSet<&str>> = HashMap::new();
+    let mut connections = vec![HashSet::new(); vertices.len()];
     for l in lines {
-        connections.entry(l.0).or_default().extend(&l.1);
+        connections[l.0].extend(&l.1);
         for o in l.1 {
-            connections.entry(o).or_default().insert(l.0);
+            connections[o].insert(l.0);
         }
     }
 
-    // get list of all vertices
-    let vertices = connections.keys().map(|k| *k).collect::<Vec<_>>();
-
     // use Brandes' algorithm to find the top 6 vertices with the highest
     // betweenness centrality
-    let top = brandes(&vertices, &connections, 6);
+    let top = brandes(vertices.len(), &connections, 6);
 
     // remove all connections between all top nodes
     for t in &top {
         for u in &top {
-            connections.entry(t).or_default().remove(u);
+            connections[*t].remove(u);
         }
     }
 
@@ -134,7 +126,7 @@ fn main() {
     // it with the number of vertices not belonging to this cluster
     let mut visited = HashSet::new();
     dfs(
-        connections.keys().next().unwrap(),
+        0,
         &connections,
         &mut visited,
     );
