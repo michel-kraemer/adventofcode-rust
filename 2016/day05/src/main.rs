8f37d6f3315f33f76072dea409678d5efebdc528
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-12-29 23:46:49 +0100

    2016: Optimize day 5

--- 2016/day05/src/main.rs
+++ 2016/day05/src/main.rs
@@ -1,6 +1,10 @@
 use std::fs;
 
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
 fn main() {
+    let block_size = 10000;
+
     for part1 in [true, false] {
         let input = fs::read_to_string("input.txt").expect("Could not read file");
         let input = input.trim();
@@ -9,19 +13,26 @@ fn main() {
         let mut i = 0;
         let mut ci = 0;
         while ci < 8 {
-            let pw = format!("{}{}", input, i);
-            i += 1;
-            let digest = md5::compute(pw);
-            let hex = format!("{:x}", digest);
-            if hex.starts_with("00000") {
-                let c = hex.chars().nth(5).unwrap();
+            let block = (i..i + block_size)
+                .into_par_iter()
+                .map(|i| {
+                    let pw = format!("{}{}", input, i);
+                    let digest = md5::compute(pw);
+                    format!("{:x}", digest)
+                })
+                .filter(|h| h.starts_with("00000"))
+                .collect::<Vec<_>>();
+            i += block_size;
+
+            for hex in block {
+                let c = hex.as_bytes()[5];
                 if part1 {
-                    result[ci] = c;
+                    result[ci] = c as char;
                     ci += 1;
                 } else if c.is_ascii_digit() {
-                    let pos = c.to_digit(10).unwrap();
+                    let pos = c - b'0';
                     if pos < 8 && result[pos as usize] == ' ' {
-                        result[pos as usize] = hex.chars().nth(6).unwrap();
+                        result[pos as usize] = hex.as_bytes()[6] as char;
                         ci += 1;
                     }
                 }
