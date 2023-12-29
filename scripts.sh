9d90bc6d7499b1ed4ae35e4870ef5a07c7dc519c
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-12-29 22:50:32 +0100

    Add scripts.sh

---
+++ scripts.sh
@@ -0,0 +1,13 @@
+# based on script.sh from HyperNeutrino:
+# https://github.com/hyper-neutrino/advent-of-code/blob/main/scripts.sh
+
+export AOC_COOKIE="..." # get this from the cookies tab in network tools on the AOC website
+
+function aoc-load () {
+    if [ $1 ]
+    then
+        curl --cookie "session=$AOC_COOKIE" https://adventofcode.com/$1/day/$2/input > input.txt
+    else
+        curl --cookie "session=$AOC_COOKIE" "$(echo `date +https://adventofcode.com/%Y/day/%d/input` | sed 's/\/0/\//g')" > input.txt
+    fi
+}
