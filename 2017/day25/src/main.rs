ab230484efccb2da10cdd31df4a531f1e392f830
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-01-11 21:21:46 +0100

    2017: Day 25

---
+++ 2017/day25/src/main.rs
@@ -0,0 +1,88 @@
+use std::{collections::HashMap, fs};
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let blocks = input.split("\n\n").collect::<Vec<_>>();
+
+    let steps = blocks[0]
+        .split(' ')
+        .nth(8)
+        .unwrap()
+        .parse::<usize>()
+        .unwrap();
+
+    let mut instructions = Vec::new();
+    for b in &blocks[1..] {
+        let mut i = b.lines();
+        let in_state = i
+            .next()
+            .unwrap()
+            .strip_suffix(':')
+            .unwrap()
+            .split(' ')
+            .last()
+            .unwrap();
+        for _ in 0..2 {
+            let f = i
+                .next()
+                .unwrap()
+                .strip_suffix(':')
+                .unwrap()
+                .split(' ')
+                .last()
+                .unwrap()
+                .parse::<usize>()
+                .unwrap();
+            let f_write = i
+                .next()
+                .unwrap()
+                .strip_suffix('.')
+                .unwrap()
+                .split(' ')
+                .last()
+                .unwrap()
+                .parse::<usize>()
+                .unwrap();
+            let f_move = i
+                .next()
+                .unwrap()
+                .strip_suffix('.')
+                .unwrap()
+                .split(' ')
+                .last()
+                .unwrap();
+            let f_continue = i
+                .next()
+                .unwrap()
+                .strip_suffix('.')
+                .unwrap()
+                .split(' ')
+                .last()
+                .unwrap();
+            instructions.push((in_state, f, f_write, f_move, f_continue));
+        }
+    }
+
+    let mut tape = HashMap::new();
+    let mut cursor = 0;
+    let mut state = "A";
+
+    for _ in 0..steps {
+        let current = tape.entry(cursor).or_insert(0);
+        for i in &instructions {
+            if state == i.0 && *current == i.1 {
+                *current = i.2;
+                if i.3 == "left" {
+                    cursor -= 1;
+                } else {
+                    cursor += 1;
+                }
+                state = i.4;
+                break;
+            }
+        }
+    }
+
+    let sum: usize = tape.values().sum();
+    println!("{}", sum);
+}
