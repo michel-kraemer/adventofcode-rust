65b625c6ce6a288eaf76fa29f7d727717e6740dc
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-02-18 11:26:15 +0100

    2019: Clean up day 17

--- 2019/day17/src/main.rs
+++ 2019/day17/src/main.rs
@@ -2,22 +2,16 @@ use std::{collections::HashMap, fs};
 
 struct Machine {
     memory: Vec<i64>,
-    input1: i64,
-    input1_used: bool,
     i: usize,
     relative_base: i64,
-    ii: usize,
 }
 
 impl Machine {
-    fn new(memory: &Vec<i64>, input1: i64) -> Self {
+    fn new(memory: &Vec<i64>) -> Self {
         Machine {
             memory: memory.to_owned(),
-            input1,
-            input1_used: true,
             i: 0,
             relative_base: 0,
-            ii: 0,
         }
     }
 
@@ -27,7 +21,7 @@ impl Machine {
         }
     }
 
-    fn run(&mut self, input2: &str) -> Option<i64> {
+    fn run(&mut self, mut read_input: impl FnMut() -> i64) -> Option<i64> {
         loop {
             self.ensure_memory(self.i);
             let code = self.memory[self.i];
@@ -86,13 +80,7 @@ impl Machine {
 
                 3 => {
                     // read input
-                    if self.input1_used {
-                        out!(1, input2.chars().nth(self.ii).unwrap() as i64);
-                        self.ii += 1;
-                    } else {
-                        out!(1, self.input1);
-                        self.input1_used = true;
-                    }
+                    out!(1, read_input());
                     self.i += 2;
                 }
 
@@ -146,35 +134,33 @@ impl Machine {
     }
 }
 
-fn find_new_dir(x: i32, y: i32, dir: (i32, i32), grid: &HashMap<(i32, i32), i64>) -> Option<char> {
+enum Turn {
+    Left,
+    Right,
+}
+
+struct Move(Turn, usize);
+
+fn find_turn(
+    x: i32,
+    y: i32,
+    dir: (i32, i32),
+    grid: &HashMap<(i32, i32), u8>,
+) -> Option<(Turn, (i32, i32))> {
     for d in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
         let nx = x + d.0;
         let ny = y + d.1;
-        if let Some(35) = grid.get(&(nx, ny)) {
-            let new_dir = match (dir, d) {
-                ((0, -1), (0, -1)) => continue,
-                ((0, -1), (0, 1)) => continue,
-                ((0, -1), (1, 0)) => 'R',
-                ((0, -1), (-1, 0)) => 'L',
-
-                ((0, 1), (0, 1)) => continue,
-                ((0, 1), (0, -1)) => continue,
-                ((0, 1), (-1, 0)) => 'R',
-                ((0, 1), (1, 0)) => 'L',
-
-                ((-1, 0), (-1, 0)) => continue,
-                ((-1, 0), (1, 0)) => continue,
-                ((-1, 0), (0, -1)) => 'R',
-                ((-1, 0), (0, 1)) => 'L',
-
-                ((1, 0), (1, 0)) => continue,
-                ((1, 0), (-1, 0)) => continue,
-                ((1, 0), (0, 1)) => 'R',
-                ((1, 0), (0, -1)) => 'L',
-
-                _ => panic!(),
-            };
-            return Some(new_dir);
+        if let Some(b'#') = grid.get(&(nx, ny)) {
+            if dir == d || dir == (-d.0, -d.1) {
+                continue;
+            }
+            if dir == (d.1, -d.0) {
+                return Some((Turn::Right, d));
+            }
+            if dir == (-d.1, d.0) {
+                return Some((Turn::Left, d));
+            }
+            panic!("Invalid direction");
         }
     }
     None
@@ -182,91 +168,78 @@ fn find_new_dir(x: i32, y: i32, dir: (i32, i32), grid: &HashMap<(i32, i32), i64>
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
-    let mut memory = input
+    let memory = input
         .trim()
         .split(',')
         .map(|i| i.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
 
+    // run machine and receive map
     let mut grid = HashMap::new();
 
     let mut x = 0;
     let mut y = 0;
-    let mut dir_x = 0;
-    let mut dir_y = -1;
 
     let mut robot_pos = (0, 0);
 
-    let mut robot = Machine::new(&memory, 0);
-    loop {
-        if let Some(o) = robot.run("") {
-            if o != 35 && o != 46 && o != 10 {
-                println!("{}", o);
+    let mut robot = Machine::new(&memory);
+    while let Some(o) = robot.run(|| 0) {
+        match o as u8 {
+            b'\n' => {
+                y += 1;
+                x = 0;
+            }
+            b'<' | b'>' | b'^' | b'v' => {
                 robot_pos = (x, y);
+                grid.insert((x, y), o as u8);
+                x += 1;
             }
-            match o {
-                10 => {
-                    y += 1;
-                    x = 0;
-                }
-                _ => {
-                    grid.insert((x, y), o);
-                    x += 1;
-                }
+            b'#' | b'.' => {
+                grid.insert((x, y), o as u8);
+                x += 1;
             }
-        } else {
-            break;
+            _ => panic!("Invalid output {}", o),
         }
     }
 
-    let mut sum = 0;
-    for (k, &v) in &grid {
-        if v == 35 {
-            let x = k.0;
-            let y = k.1;
-            let mut c = 0;
-            for d in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-                let nx = x + d.0;
-                let ny = y + d.1;
-                if let Some(35) = grid.get(&(nx, ny)) {
-                    c += 1;
-                }
+    // part 1: count intersections in map
+    let mut total_intersections = 0;
+    for ((x, y), _) in grid.iter().filter(|(_, &v)| v == b'#') {
+        let mut c = 0;
+        for d in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+            let nx = x + d.0;
+            let ny = y + d.1;
+            if let Some(b'#') = grid.get(&(nx, ny)) {
+                c += 1;
             }
-            if c == 4 {
-                sum += x * y;
-            }
+        }
+        if c == 4 {
+            total_intersections += x * y;
         }
     }
 
-    println!("{}", sum);
+    println!("{}", total_intersections);
 
-    println!("{:?}", robot_pos);
-    let c = (*grid.get(&robot_pos).unwrap() as u8) as char;
-    println!("{}", c);
-
+    // part 2 ...
+    // start at the robot's position and trace the scaffolding
     x = robot_pos.0;
     y = robot_pos.1;
-    let mut dir = (0, -1);
-    let mut s = String::new();
-    while let Some(dir_instr) = find_new_dir(x, y, dir, &grid) {
-        dir = match (dir, dir_instr) {
-            ((0, 1), 'R') => (-1, 0),
-            ((0, -1), 'R') => (1, 0),
-            ((1, 0), 'R') => (0, 1),
-            ((-1, 0), 'R') => (0, -1),
+    let mut dir = match grid.get(&robot_pos) {
+        Some(b'^') => (0, -1),
+        Some(b'v') => (0, 1),
+        Some(b'>') => (1, 0),
+        Some(b'<') => (-1, 0),
+        _ => panic!("Invalid robot position"),
+    };
+    let mut trace = Vec::new();
+    while let Some((turn, new_dir)) = find_turn(x, y, dir, &grid) {
+        dir = new_dir;
 
-            ((0, 1), 'L') => (1, 0),
-            ((0, -1), 'L') => (-1, 0),
-            ((1, 0), 'L') => (0, -1),
-            ((-1, 0), 'L') => (0, 1),
-
-            _ => panic!(),
-        };
-
+        // trace until next turn (or end of scaffolding)
         let mut nx = x + dir.0;
         let mut ny = y + dir.1;
         let mut steps = 0;
-        while let Some(35) = grid.get(&(nx, ny)) {
+        while let Some(b'#') = grid.get(&(nx, ny)) {
             x = nx;
             y = ny;
             nx += dir.0;
@@ -274,39 +247,28 @@ fn main() {
             steps += 1;
         }
 
-        if !s.is_empty() {
-            s.push(',');
-        }
-        s.push(dir_instr);
-        s.push(',');
-        s.push_str(&steps.to_string());
+        trace.push(Move(turn, steps));
     }
-
-    println!("{}", s);
 
     // => "R,4,L,10,L,10,L,8,R,12,R,10,R,4,R,4,L,10,L,10,L,8,R,12,R,10,R,4,R,4,L,10,L,10,L,8,L,8,R,10,R,4,L,8,R,12,R,10,R,4,L,8,L,8,R,10,R,4,R,4,L,10,L,10,L,8,L,8,R,10,R,4";
 
-    let movement = "A,B,A,B,A,C,B,C,A,C";
+    let main = "A,B,A,B,A,C,B,C,A,C";
 
     let a = "R,4,L,10,L,10";
     let b = "L,8,R,12,R,10,R,4";
     let c = "L,8,L,8,R,10,R,4";
+    let video = "n";
 
-    let mut input = String::new();
-    input.push_str(movement);
-    input.push('\n');
-    input.push_str(a);
-    input.push('\n');
-    input.push_str(b);
-    input.push('\n');
-    input.push_str(c);
-    input.push('\n');
-    input.push('n');
-    input.push('\n');
+    let input = [main, a, b, c, video, ""].join("\n");
 
+    // wake up robot
     robot.memory[0] = 2;
+
+    // Provide instructions to robot and wait until it has visited every
+    // scaffolding piece. Read final output (= collected dust).
     let mut last_output = 0;
-    while let Some(o) = robot.run(&input) {
+    let mut input_iter = input.chars().map(|c| c as i64);
+    while let Some(o) = robot.run(|| input_iter.next().unwrap()) {
         last_output = o;
     }
 
