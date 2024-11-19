383f33d778081436f91d30c339f159dc265f4f6d
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-11-19 06:37:11 +0100

    Optimize revisited day 16 of 2023

--- 2023/day16/src/main.rs
+++ 2023/day16/src/main.rs
@@ -1,17 +1,26 @@
-use std::{
-    collections::{HashSet, VecDeque},
-    fs,
-};
+use std::{collections::VecDeque, fs};
+
+fn diri(b: (i32, i32, i32, i32)) -> usize {
+    if b.2 == 1 {
+        0
+    } else if b.2 == -1 {
+        1
+    } else if b.3 == 1 {
+        2
+    } else {
+        3
+    }
+}
 
 fn energize(start: (i32, i32, i32, i32), grid: &[Vec<char>]) -> usize {
     let mut queue = VecDeque::new();
     queue.push_back(start);
 
-    let mut seen = HashSet::new();
-    let mut visited = HashSet::new();
+    let mut seen = vec![vec![[false; 4]; grid[0].len()]; grid.len()];
+    let mut visited = vec![vec![false; grid[0].len()]; grid.len()];
 
     'outer: while let Some(mut b) = queue.pop_front() {
-        visited.insert((b.0, b.1));
+        visited[b.1 as usize][b.0 as usize] = true;
 
         // performance optimization shortcut: continue straight as far as possible
         while grid[b.1 as usize][b.0 as usize] == '.' {
@@ -20,11 +29,10 @@ fn energize(start: (i32, i32, i32, i32), grid: &[Vec<char>]) -> usize {
             if b.0 < 0 || b.1 < 0 || b.0 == grid[0].len() as i32 || b.1 == grid.len() as i32 {
                 continue 'outer;
             }
-            visited.insert((b.0, b.1));
+            visited[b.1 as usize][b.0 as usize] = true;
         }
 
         let (nb1, nb2) = match grid[b.1 as usize][b.0 as usize] {
-            '.' => (Some(b), None),
             '/' => (Some((b.0, b.1, -b.3, -b.2)), None),
             '\\' => (Some((b.0, b.1, b.3, b.2)), None),
             '|' => {
@@ -47,19 +55,20 @@ fn energize(start: (i32, i32, i32, i32), grid: &[Vec<char>]) -> usize {
         for mut nb in [nb1, nb2].into_iter().flatten() {
             nb.0 += nb.2;
             nb.1 += nb.3;
-            if !seen.contains(&nb)
-                && nb.0 >= 0
+            let di = diri(nb);
+            if nb.0 >= 0
                 && nb.1 >= 0
                 && nb.0 < grid[0].len() as i32
                 && nb.1 < grid.len() as i32
+                && !seen[nb.1 as usize][nb.0 as usize][di]
             {
-                seen.insert(nb);
+                seen[nb.1 as usize][nb.0 as usize][di] = true;
                 queue.push_back(nb);
             }
         }
     }
 
-    visited.len()
+    visited.into_iter().flatten().filter(|&v| v).count()
 }
 
 fn main() {
