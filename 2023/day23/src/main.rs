a1d7530ae73bce8c5a2642863f4924cce041ebd7
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-11-25 19:50:17 +0100

    Optimize day 23 of 2023

--- 2023/day23/src/main.rs
+++ 2023/day23/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{HashSet, VecDeque},
+    collections::{HashMap, HashSet, VecDeque},
     fs,
 };
 
@@ -11,26 +11,19 @@ struct State {
     last_junction: (i32, i32),
 }
 
-type Grid<T> = Vec<Vec<T>>;
-
-fn dfs(
-    pos: (i32, i32),
-    end: (i32, i32),
-    edges: &Grid<Vec<(i32, i32, usize)>>,
-    seen: &mut Grid<bool>,
-) -> usize {
-    seen[pos.1 as usize][pos.0 as usize] = true;
-    let r = edges[pos.1 as usize][pos.0 as usize]
+fn dfs(pos: usize, end: usize, edges: &[Vec<(usize, usize)>], seen: &mut [bool]) -> usize {
+    seen[pos] = true;
+    let r = edges[pos]
         .iter()
         .map(|n| {
-            if seen[n.1 as usize][n.0 as usize] {
+            if seen[n.0] {
                 0
-            } else if (n.0, n.1) == end {
-                n.2
+            } else if n.0 == end {
+                n.1
             } else {
-                let s = dfs((n.0, n.1), end, edges, seen);
+                let s = dfs(n.0, end, edges, seen);
                 if s > 0 {
-                    n.2 + s
+                    n.1 + s
                 } else {
                     0
                 }
@@ -38,7 +31,7 @@ fn dfs(
         })
         .max()
         .unwrap();
-    seen[pos.1 as usize][pos.0 as usize] = false;
+    seen[pos] = false;
     r
 }
 
@@ -46,18 +39,24 @@ fn insert_edge(
     a: (i32, i32),
     b: (i32, i32),
     steps: usize,
-    edges: &mut Grid<Vec<(i32, i32, usize)>>,
+    junctions: &mut HashMap<(i32, i32), usize>,
+    edges: &mut [Vec<(usize, usize)>],
+    w: usize,
     part1: bool,
 ) {
-    let p1 = (b.0, b.1, steps);
-    if !edges[a.1 as usize][a.0 as usize].contains(&p1) {
-        edges[a.1 as usize][a.0 as usize].push(p1);
+    let bil = junctions.len();
+    let bi = *junctions.entry(b).or_insert(bil);
+    let p1 = (bi, steps);
+    if !edges[a.1 as usize * w + a.0 as usize].contains(&p1) {
+        edges[a.1 as usize * w + a.0 as usize].push(p1);
     }
     if !part1 {
+        let ail = junctions.len();
+        let ai = *junctions.entry(a).or_insert(ail);
         // for part 2, edges must be bidirectional
-        let p2 = (a.0, a.1, steps);
-        if !edges[b.1 as usize][b.0 as usize].contains(&p2) {
-            edges[b.1 as usize][b.0 as usize].push(p2);
+        let p2 = (ai, steps);
+        if !edges[b.1 as usize * w + b.0 as usize].contains(&p2) {
+            edges[b.1 as usize * w + b.0 as usize].push(p2);
         }
     }
 }
@@ -65,15 +64,19 @@ fn insert_edge(
 fn main() {
     for part1 in [true, false] {
         let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let grid = input
-            .lines()
-            .map(|l| l.chars().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+        let mut grid = Vec::<u8>::new();
+        let mut w = 0;
+        let mut h = 0;
+        for l in input.lines() {
+            h += 1;
+            w = l.as_bytes().len();
+            grid.extend(l.as_bytes());
+        }
 
         // find start
         let mut start = (0i32, 0i32);
-        for x in 0..grid[0].len() {
-            if grid[0][x] == '.' {
+        for (x, c) in grid.iter().enumerate().take(w) {
+            if *c == b'.' {
                 start = (x as i32, 0i32);
                 break;
             }
@@ -81,9 +84,9 @@ fn main() {
 
         // find end
         let mut end = (0i32, 0i32);
-        for x in 0..grid[grid.len() - 1].len() {
-            if grid[grid.len() - 1][x] == '.' {
-                end = (x as i32, grid.len() as i32 - 1);
+        for (x, c) in grid.iter().skip((h - 1) * w).enumerate() {
+            if *c == b'.' {
+                end = (x as i32, h as i32 - 1);
                 break;
             }
         }
@@ -99,14 +102,21 @@ fn main() {
         let mut queue = VecDeque::new();
         queue.push_back(state);
 
-        let mut junctions = HashSet::new();
-        junctions.insert(start);
-        let mut edges = vec![vec![Vec::<(i32, i32, usize)>::new(); grid[0].len()]; grid.len()];
+        let mut junctions = HashMap::new();
+        junctions.insert(start, 0);
+        let mut edges = vec![Vec::<(usize, usize)>::new(); w * h];
         while let Some(mut s) = queue.pop_front() {
             // insert edge to the end
             if s.pos == end {
-                junctions.insert(s.pos);
-                insert_edge(s.last_junction, s.pos, s.steps, &mut edges, part1);
+                insert_edge(
+                    s.last_junction,
+                    s.pos,
+                    s.steps,
+                    &mut junctions,
+                    &mut edges,
+                    w,
+                    part1,
+                );
                 continue;
             }
 
@@ -115,10 +125,10 @@ fn main() {
             for dir in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
                 let nx = s.pos.0 + dir.0;
                 let ny = s.pos.1 + dir.1;
-                if nx < 0 || ny < 0 || nx >= grid[0].len() as i32 || ny >= grid.len() as i32 {
+                if nx < 0 || ny < 0 || nx >= w as i32 || ny >= h as i32 {
                     continue;
                 }
-                if grid[ny as usize][nx as usize] == '#' {
+                if grid[ny as usize * w + nx as usize] == b'#' {
                     continue;
                 }
                 c += 1;
@@ -126,15 +136,24 @@ fn main() {
 
             // if we're at a junction, ...
             if c > 2 {
+                let have_seen = junctions.contains_key(&s.pos);
+
                 // ... insert edge to the last junction
-                insert_edge(s.last_junction, s.pos, s.steps, &mut edges, part1);
+                insert_edge(
+                    s.last_junction,
+                    s.pos,
+                    s.steps,
+                    &mut junctions,
+                    &mut edges,
+                    w,
+                    part1,
+                );
 
                 // if we haven't seen this junction before, make it the current
-                // junction and reset the steps
-                if junctions.contains(&s.pos) {
+                // junction and reset steps
+                if have_seen {
                     continue;
                 }
-                junctions.insert(s.pos);
                 s.last_junction = s.pos;
                 s.steps = 0;
             }
@@ -143,26 +162,26 @@ fn main() {
             for dir in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
                 let nx = s.pos.0 + dir.0;
                 let ny = s.pos.1 + dir.1;
-                if nx < 0 || ny < 0 || nx >= grid[0].len() as i32 || ny >= grid.len() as i32 {
+                if nx < 0 || ny < 0 || nx >= w as i32 || ny >= h as i32 {
                     continue;
                 }
-                if grid[ny as usize][nx as usize] == '#' {
+                if grid[ny as usize * w + nx as usize] == b'#' {
                     continue;
                 }
                 if s.seen.contains(&(nx, ny)) {
                     continue;
                 }
                 if part1 {
-                    if dir == (1, 0) && grid[ny as usize][nx as usize] == '<' {
+                    if dir == (1, 0) && grid[ny as usize * w + nx as usize] == b'<' {
                         continue;
                     }
-                    if dir == (-1, 0) && grid[ny as usize][nx as usize] == '>' {
+                    if dir == (-1, 0) && grid[ny as usize * w + nx as usize] == b'>' {
                         continue;
                     }
-                    if dir == (0, 1) && grid[ny as usize][nx as usize] == '^' {
+                    if dir == (0, 1) && grid[ny as usize * w + nx as usize] == b'^' {
                         continue;
                     }
-                    if dir == (0, -1) && grid[ny as usize][nx as usize] == 'v' {
+                    if dir == (0, -1) && grid[ny as usize * w + nx as usize] == b'v' {
                         continue;
                     }
                 }
@@ -178,9 +197,15 @@ fn main() {
             }
         }
 
+        // compress edges
+        let mut le = vec![Vec::new(); junctions.len()];
+        for (pos, &i) in &junctions {
+            std::mem::swap(&mut le[i], &mut edges[pos.1 as usize * w + pos.0 as usize]);
+        }
+
         // perform DFS to find the longest path
-        let mut seen = vec![vec![false; grid[0].len()]; grid.len()];
-        let max = dfs(start, end, &edges, &mut seen);
+        let mut seen = vec![false; le.len()];
+        let max = dfs(0, junctions[&end], &le, &mut seen);
         println!("{}", max);
     }
 }
