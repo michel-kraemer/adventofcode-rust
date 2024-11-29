d5cd4928e8447e1204fc639a2275cb9d436caad4
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-11-29 19:46:35 +0100

    Optimize day 6 of 2023

--- 2023/day06/src/main.rs
+++ 2023/day06/src/main.rs
@@ -24,21 +24,22 @@ fn main() {
         for i in 0..times.len() {
             let time = times[i];
             let record = records[i];
-            let mut wins = 0;
 
-            // We only need to consider half of the search space because
-            // results are symmetrical. Start in the middle. (For whatever
-            // reason, counting down is faster than counting up on my computer.
-            // Tested with hyperfine.)
-            for duration in (0..(time + 1) / 2).rev() {
+            // We only need to consider half of the search space because results
+            // are symmetrical. Start in the middle and perform a binary search
+            // to find the first duration that breaks the record.
+            let start = (time + 1) / 2;
+            let mut high = start;
+            let mut low = 0;
+            while high > low {
+                let duration = low + (high - low) / 2;
                 if duration * (time - duration) > record {
-                    wins += 2;
+                    high = duration;
                 } else {
-                    // Break as soon as we don't reach the record anymore. The
-                    // remaining tries won't succeed either.
-                    break;
+                    low = duration + 1;
                 }
             }
+            let mut wins = (start - high) * 2;
 
             // if `time` is even, we still have to consider the middle
             if time % 2 == 0 && time / 2 * (time - time / 2) > record {
