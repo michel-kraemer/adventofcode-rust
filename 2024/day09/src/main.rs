20bd87f7dfb3690342198b14c88b8bf1f8b15965
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-12-09 19:42:21 +0100

    2024: Optimize day 9

--- 2024/day09/src/main.rs
+++ 2024/day09/src/main.rs
@@ -5,7 +5,9 @@ const SPACE: usize = usize::MAX;
 #[derive(Debug, Copy, Clone)]
 struct Item {
     id: usize,
+    pos: usize,
     len: usize,
+    filled: bool,
 }
 
 fn checksum(disk: &[Item]) -> usize {
@@ -24,98 +26,114 @@ fn checksum(disk: &[Item]) -> usize {
     result
 }
 
-fn main() {
-    let input = fs::read_to_string("input.txt").expect("Could not read file");
+fn part1(mut disk: Vec<Item>) -> usize {
+    let mut result = Vec::new();
+    let mut i = disk.len() - 1;
+    let mut j = 0;
 
-    let mut orig_disk = Vec::new();
-    let mut chars = input.trim().chars();
-    let mut id = 0;
-    while let Some(file_len) = chars.next() {
-        orig_disk.push(Item {
-            id,
-            len: file_len.to_digit(10).unwrap() as usize,
-        });
-        id += 1;
-        if let Some(space_len) = chars.next() {
-            orig_disk.push(Item {
-                id: SPACE,
-                len: space_len.to_digit(10).unwrap() as usize,
-            });
+    while i >= j {
+        if disk[i].id != SPACE {
+            // just copy files
+            result.push(disk[i]);
         } else {
-            break;
-        }
-    }
-
-    // part 1
-    let mut cloned_disk = orig_disk.clone();
-    let mut disk1 = Vec::new();
-    let mut i = 0;
-    let mut j = cloned_disk.len() - 1;
-    while i <= j {
-        if cloned_disk[i].id != SPACE {
-            disk1.push(cloned_disk[i]);
-        } else {
-            let mut space_len = cloned_disk[i].len;
+            // we found space that needs to be filled: copy blocks from
+            // files from the back
+            let mut space_len = disk[i].len;
             while space_len > 0 {
-                let mut f = cloned_disk[j];
-                while f.id == SPACE {
-                    j -= 1;
-                    f = cloned_disk[j];
+                // first file from back
+                if j >= i {
+                    break;
                 }
+                let f = disk[j];
 
                 if f.len <= space_len {
+                    // file fits completely into space
                     space_len -= f.len;
-                    j -= 1;
-                    disk1.push(f);
+                    result.push(f);
+                    j += 2; // next file - skip space
                 } else {
-                    cloned_disk[j].len -= space_len;
-                    disk1.push(Item {
+                    // file is larger than space: copy as many blocks as possible
+                    result.push(Item {
                         id: f.id,
+                        pos: result.len(),
                         len: space_len,
+                        filled: false,
                     });
+                    disk[j].len -= space_len;
                     space_len = 0;
                 }
             }
         }
-        i += 1;
+
+        i -= 1;
     }
 
-    let total1 = checksum(&disk1);
-    println!("{}", total1);
+    checksum(&result)
+}
 
-    // part 2
-    let mut disk2 = orig_disk;
-    let mut j = disk2.len() - 1;
-    loop {
-        if disk2[j].id != SPACE {
-            let file_len = disk2[j].len;
-            for i in 0..j {
-                if disk2[i].id == SPACE && disk2[i].len >= disk2[j].len {
-                    let space_len = disk2[i].len;
-                    disk2[i] = Item {
-                        id: disk2[j].id,
-                        len: file_len,
-                    };
-                    disk2[j].id = SPACE;
-                    if space_len > file_len {
-                        disk2.insert(
-                            i + 1,
-                            Item {
-                                id: SPACE,
-                                len: space_len - file_len,
-                            },
-                        );
-                    }
-                    break;
+fn part2(mut disk: Vec<Item>) -> usize {
+    let mut first_space = disk.len() - 2;
+    let mut j = 0;
+    while j < disk.len() {
+        // get next file from back
+        let f = disk[j];
+
+        // find space where the file would fit completely
+        let mut found_first_space = false;
+        for i in (j + 1..=first_space).rev().step_by(2) {
+            if !found_first_space && disk[i].id == SPACE && disk[i].len > 0 {
+                first_space = i;
+                found_first_space = true;
+            }
+            if disk[i].id == SPACE && disk[i].len >= f.len {
+                // move file
+                disk[j].pos = disk[i].pos;
+
+                // make space before the moved file larger and merge with space after it
+                disk[j + 1].len += disk[j].len;
+                if j > 0 && !disk[j - 1].filled {
+                    disk[j + 1].len += disk[j - 1].len;
+                    disk[j - 1].len = 0;
                 }
+
+                // make new space smaller; mark it as filled so we won't
+                // merge it later
+                disk[i].len -= disk[j].len;
+                disk[i].filled = true;
+
+                break;
             }
         }
-        if j == 0 {
-            break;
-        }
-        j -= 1;
+
+        j += 2; // next file - skip space
     }
 
-    let total2 = checksum(&disk2);
-    println!("{}", total2);
+    disk.sort_by_key(|i| i.pos); // stable sort!
+
+    checksum(&disk)
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+
+    let mut disk = Vec::new();
+    let bytes = input.trim().as_bytes();
+    let mut i = bytes.len() - 1;
+    loop {
+        let id = if i % 2 == 0 { i / 2 } else { SPACE };
+        let len = (bytes[i] - b'0') as usize;
+        disk.push(Item {
+            id,
+            pos: i,
+            len,
+            filled: false,
+        });
+        if i == 0 {
+            break;
+        }
+        i -= 1;
+    }
+
+    println!("{}", part1(disk.clone()));
+    println!("{}", part2(disk));
 }
