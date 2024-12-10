a5ff45486597e73fe2db39c0fb83a0647b9bbbce
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-12-10 12:12:52 +0100

    2024: Optimize day 10

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::VecDeque;
 use std::fs;
 
 fn main() {
@@ -16,37 +16,40 @@ fn main() {
     for y in 0..height {
         for x in 0..width {
             if grid[y * width + x] == b'0' {
-                trailheads.push((x as i32, y as i32, b'0'));
+                trailheads.push((x as i32, y as i32));
             }
         }
     }
 
     let mut total1 = 0;
     let mut total2 = 0;
+    let mut queue = VecDeque::new();
+    let mut seen = vec![false; grid.len()];
     for t in trailheads {
-        let mut queue = VecDeque::new();
         queue.push_back(t);
-        let mut unique = HashSet::new();
-        let mut rating = 0;
+        seen.fill(false);
         while let Some(pos) = queue.pop_front() {
-            if pos.2 == b'9' {
-                unique.insert((pos.0, pos.1));
-                rating += 1;
+            let i = pos.1 as usize * width + pos.0 as usize;
+            let c = grid[i];
+            if c == b'9' {
+                if !seen[i] {
+                    total1 += 1;
+                    seen[i] = true;
+                }
+                total2 += 1;
                 continue;
             }
-            for d in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
-                let nx = pos.0 + d.0;
-                let ny = pos.1 + d.1;
+            for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+                let nx = pos.0 + dir.0;
+                let ny = pos.1 + dir.1;
                 if nx >= 0 && ny >= 0 && nx < width as i32 && ny < height as i32 {
                     let d = grid[ny as usize * width + nx as usize];
-                    if d == pos.2 + 1 {
-                        queue.push_back((nx, ny, d));
+                    if d == c + 1 {
+                        queue.push_back((nx, ny));
                     }
                 }
             }
         }
-        total1 += unique.len();
-        total2 += rating;
     }
 
     println!("{}", total1);
