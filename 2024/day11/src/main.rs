8cf35fb70fc8e9a42eb6e92708e69a377b692277
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-12-11 18:11:39 +0100

    2024: Optimize day 11

--- 2024/day11/src/main.rs
+++ 2024/day11/src/main.rs
@@ -1,29 +1,30 @@
 use std::collections::HashMap;
 use std::fs;
 
-fn apply(n: u64, i: usize, seen: &mut HashMap<(u64, usize), usize>) -> usize {
+fn blink(n: u64, i: usize, cache: &mut HashMap<(u64, usize), usize>) -> usize {
     if i == 0 {
-        return 1;
-    } else if let Some(cached) = seen.get(&(n, i)) {
-        return *cached;
-    }
-
-    let r = if n == 0 {
-        apply(1, i - 1, seen)
+        1
+    } else if n == 0 {
+        blink(1, i - 1, cache)
     } else {
         let ndigits = n.ilog10() + 1;
         if ndigits % 2 == 0 {
-            let mask = 10u64.pow(ndigits / 2);
-            let l = n / mask;
-            let r = n % mask;
-            apply(l, i - 1, seen) + apply(r, i - 1, seen)
+            // only caching stones with an even number of digits leads to fewer
+            // hash map lookups and is apparently beneficial for performance
+            if let Some(cached) = cache.get(&(n, i)) {
+                *cached
+            } else {
+                let mask = 10u64.pow(ndigits / 2);
+                let l = n / mask;
+                let r = n % mask;
+                let s = blink(l, i - 1, cache) + blink(r, i - 1, cache);
+                cache.insert((n, i), s);
+                s
+            }
         } else {
-            apply(n * 2024, i - 1, seen)
+            blink(n * 2024, i - 1, cache)
         }
-    };
-
-    seen.insert((n, i), r);
-    r
+    }
 }
 
 fn main() {
@@ -31,9 +32,9 @@ fn main() {
         let input = fs::read_to_string("input.txt").expect("Could not read file");
 
         let mut total = 0;
-        let mut seen = HashMap::new();
+        let mut cache = HashMap::with_capacity(5000);
         for s in input.split_whitespace() {
-            total += apply(s.parse().unwrap(), if part1 { 25 } else { 75 }, &mut seen);
+            total += blink(s.parse().unwrap(), if part1 { 25 } else { 75 }, &mut cache);
         }
 
         println!("{}", total);
