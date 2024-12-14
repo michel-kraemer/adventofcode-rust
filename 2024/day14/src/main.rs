26db76dca15f869ced8e226a2acd6084a99f5f03
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2024-12-14 11:09:25 +0100

    2024: Optimize day 14

--- 2024/day14/src/main.rs
+++ 2024/day14/src/main.rs
@@ -1,13 +1,15 @@
 use std::fs;
 
 #[allow(unused)]
-fn print_grid(grid: &[usize], w: usize, h: usize) {
+fn print_grid(grid: &[u16], w: usize, h: usize) {
     for y in 0..h {
         for x in 0..w {
-            if grid[y * w + x] == 0 {
-                print!(".");
-            } else {
-                print!("#");
+            for i in (0..16).rev() {
+                if grid[y * w + x] & 1 << i > 0 {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
             }
         }
         println!();
@@ -31,8 +33,10 @@ fn main() {
     let w = 101;
     let h = 103;
     let mut grid = vec![0; w * h];
-    let mut robots = Vec::new();
+    let bw = (w + 15) / 16;
+    let mut binary_grid = vec![0u16; bw * h];
 
+    let mut robots = Vec::new();
     for l in lines {
         let (p, v) = l.split_once(' ').unwrap();
         let (px, py) = p[2..]
@@ -45,32 +49,23 @@ fn main() {
             .unwrap();
         robots.push((px, py, vx, vy));
         grid[py as usize * w + px as usize] += 1;
+        binary_grid[py as usize * bw + px as usize / 16] |= 1 << (15 - px % 16);
     }
 
-    let mut total1 = 0;
-    let mut total2 = 0;
+    let mut total1 = None;
+    let mut total2 = None;
     let mut steps = 0;
     'outer: loop {
         for (px, py, vx, vy) in &mut robots {
-            grid[*py as usize * w + *px as usize] -= 1;
-
-            *px += *vx;
-            if *px >= w as i32 {
-                *px -= w as i32;
+            let ni = *py as usize * w + *px as usize;
+            grid[ni] -= 1;
+            if grid[ni] == 0 {
+                binary_grid[*py as usize * bw + *px as usize / 16] &= !(1 << (15 - *px % 16));
             }
-            if *px < 0 {
-                *px += w as i32;
-            }
-
-            *py += *vy;
-            if *py >= h as i32 {
-                *py -= h as i32;
-            }
-            if *py < 0 {
-                *py += h as i32;
-            }
-
+            *px = (*px + *vx).rem_euclid(w as i32);
+            *py = (*py + *vy).rem_euclid(h as i32);
             grid[*py as usize * w + *px as usize] += 1;
+            binary_grid[*py as usize * bw + *px as usize / 16] |= 1 << (15 - *px % 16);
         }
 
         steps += 1;
@@ -80,29 +75,24 @@ fn main() {
             let t2 = count(&grid, w, 0, w / 2, (h + 1) / 2, h);
             let t3 = count(&grid, w, (w + 1) / 2, w, 0, h / 2);
             let t4 = count(&grid, w, (w + 1) / 2, w, (h + 1) / 2, h);
-            total1 = t1 * t2 * t3 * t4;
-            if total2 > 0 {
+            total1 = Some(t1 * t2 * t3 * t4);
+            if total2.is_some() {
                 break;
             }
         }
 
         // look for at least 16 robots in a row. this should be our christmas tree.
-        let min_run = 16;
         for y in 0..h {
-            for x in 0..w - min_run {
-                let mut found = true;
-                for i in 0..min_run {
-                    if grid[y * w + x + i] == 0 {
-                        found = false;
-                        break;
-                    }
-                }
-                if found {
+            for x in 0..bw - 1 {
+                if binary_grid[y * bw + x].trailing_ones()
+                    + binary_grid[y * bw + x + 1].leading_ones()
+                    >= 16
+                {
                     // use print_grid to check if it's really a christmas tree
-                    // print_grid(&grid, w, h);
+                    // print_grid(&binary_grid, bw, h);
 
-                    total2 = steps;
-                    if total1 > 0 {
+                    total2 = Some(steps);
+                    if total1.is_some() {
                         break 'outer;
                     }
                 }
@@ -110,6 +100,6 @@ fn main() {
         }
     }
 
-    println!("{}", total1);
-    println!("{}", total2);
+    println!("{}", total1.unwrap());
+    println!("{}", total2.unwrap());
 }
