1a9beecb702d22a4acbb03e09b4bb85e01c8fa90
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-03 08:27:12 +0100

    2025: Optimize day 3

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -5,21 +5,26 @@ fn main() {
 
     let mut total1 = 0;
     let mut total2 = 0;
+    let mut dp = vec![0; 13];
     for l in input.lines() {
         let bytes = l.as_bytes();
-        let mut dp = vec![vec![0; bytes.len() + 1]; 13];
+        if (bytes.len() + 1) * 13 > dp.len() {
+            dp.resize((bytes.len() + 1) * 13, 0);
+        } else {
+            dp.fill(0);
+        }
         let mut mul = 1;
         for len in 1..=12 {
             let mut max = 0;
             for (i, &b) in bytes.iter().enumerate().take(bytes.len() - len + 1).rev() {
                 let d = (b - b'0') as u64;
-                max = max.max(d * mul + dp[len - 1][i + 1]);
-                dp[len][i] = max;
+                max = max.max(d * mul + dp[(len - 1) * 13 + i + 1]);
+                dp[len * 13 + i] = max;
             }
             mul *= 10;
         }
-        total1 += dp[2][0];
-        total2 += dp[12][0];
+        total1 += dp[2 * 13];
+        total2 += dp[12 * 13];
     }
 
     println!("{total1}");
