6df9f7f266f38df964c1b8a1dc55c5c2aee74e02
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-03 10:22:30 +0100

    2025: Optimize day 2

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
@@ -10,50 +10,56 @@ fn main() {
         let (lo, hi) = range.split_once("-").unwrap();
         max_len = max_len.max(lo.len());
         max_len = max_len.max(hi.len());
-        let lo = lo.parse::<i64>().unwrap();
-        let hi = hi.parse::<i64>().unwrap();
+        let lo = lo.parse::<u64>().unwrap();
+        let hi = hi.parse::<u64>().unwrap();
         ranges.push(lo..=hi);
     }
 
+    // ranges are non-overlapping, so we can sort them
+    ranges.sort_by_key(|r| *r.start());
+
     // Generate all possible numbers with repeated sequences of digits. Keep
     // track of those that only consist of two sequences (for part 1).
-    let mut all_part1 = HashSet::new();
-    let mut all = HashSet::new();
-    let mut mul: i64 = 10;
-    let mut ilen = 1;
-    let mut chunks = max_len;
-    for i in 1.. {
-        if i == mul {
+    let mut all = Vec::new();
+    for l in 2..=max_len {
+        let mut mul: u64 = 10;
+        for sl in 1..=l / 2 {
+            if l.is_multiple_of(sl) {
+                for i in mul / 10..mul {
+                    let mut n = 0;
+                    for _ in 0..l / sl {
+                        n *= mul;
+                        n += i;
+                    }
+                    all.push((n, !(l.is_multiple_of(2) && sl == l / 2)));
+                }
+            }
             mul *= 10;
-            ilen += 1;
-            chunks = max_len / ilen;
-            if chunks == 1 {
-                break;
-            }
-        }
-        let mut m = i;
-        for c in 0..chunks - 1 {
-            m *= mul;
-            m += i;
-            if c == 0 {
-                // number consists of only two sequences
-                all_part1.insert(m);
-            }
-            all.insert(m);
         }
     }
+    all.sort(); // sort() is faster than sort_unstable() in this case
+    all.dedup_by_key(|a| a.0);
 
     // check which of the numbers in `all` lie in any of the ranges
+    let mut ri = ranges.iter();
+    let mut current_range = ri.next().unwrap();
     let mut total1 = 0;
     let mut total2 = 0;
-    for a in &all {
-        if ranges.iter().any(|range| range.contains(a)) {
-            if all_part1.contains(a) {
+    for (a, odd) in &all {
+        if current_range.end() < a {
+            let Some(cr) = ri.find(|r| r.end() >= a) else {
+                break;
+            };
+            current_range = cr;
+        }
+        if current_range.contains(a) {
+            if !odd {
                 total1 += a;
             }
             total2 += a;
         }
     }
+
     println!("{total1}");
     println!("{total2}");
 }
