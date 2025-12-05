2436c2fe7f98ce74fb01a14b67d21a963cb15fa9
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-05 10:33:31 +0100

    2025: Optimize day 5

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, fs};
+use std::fs;
 
 fn main() {
     // parse input
@@ -15,16 +15,18 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let ids = block2
+    let mut ids = block2
         .lines()
         .map(|l| l.parse::<i64>().unwrap())
         .collect::<Vec<_>>();
 
     ranges.sort_by(|a, b| a.start().cmp(b.start()).then(b.end().cmp(a.start())));
+    ids.sort_unstable();
 
-    // merge ranges
+    // merge ranges (and keep track of range lengths for part 2 already)
     let mut merged_ranges = Vec::new();
     let mut i = 0;
+    let mut total2 = 0;
     while i < ranges.len() {
         let mut r = ranges[i].clone();
         i += 1;
@@ -32,35 +34,36 @@ fn main() {
             r = *r.start()..=*r.end().max(ranges[i].end());
             i += 1;
         }
+        total2 += r.end() - r.start() + 1;
         merged_ranges.push(r);
     }
 
     // part 1
-    println!(
-        "{}",
-        ids.into_iter()
-            .filter_map(|id| {
-                merged_ranges
-                    .binary_search_by(|r| {
-                        if *r.start() > id {
-                            Ordering::Greater
-                        } else if *r.end() < id {
-                            Ordering::Less
-                        } else {
-                            Ordering::Equal
-                        }
-                    })
-                    .ok()
-            })
-            .count()
-    );
+    let mut ri = merged_ranges.into_iter();
+    let mut current_range = ri.next().unwrap();
+    let mut current_id = 0;
+    let mut total1 = 0;
+    while current_id < ids.len() {
+        // skip to the next range
+        if *current_range.end() < ids[current_id] {
+            let Some(cr) = ri.find(|r| *r.end() >= ids[current_id]) else {
+                break;
+            };
+            current_range = cr;
+        }
 
-    // part 2
-    println!(
-        "{}",
-        merged_ranges
-            .into_iter()
-            .map(|r| r.end() - r.start() + 1)
-            .sum::<i64>()
-    );
+        // skip to the next ID
+        while current_id < ids.len() && ids[current_id] < *current_range.start() {
+            current_id += 1;
+        }
+
+        // count all IDs that lie inside the current range
+        while current_id < ids.len() && current_range.contains(&ids[current_id]) {
+            total1 += 1;
+            current_id += 1;
+        }
+    }
+
+    println!("{total1}");
+    println!("{total2}");
 }
