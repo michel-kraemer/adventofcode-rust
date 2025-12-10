bc43439f4e965459981e635b1868b3e3a455afba
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-10 16:45:52 +0100

    2025: Optimize day 10

--- 2025/day10/src/main.rs
+++ 2025/day10/src/main.rs
@@ -1,140 +1,100 @@
 use std::collections::HashMap;
 use std::fs;
 
+struct Machine {
+    target_lights: Vec<bool>,
+    buttons: Vec<Vec<usize>>,
+    target_joltage: Vec<usize>,
+}
+
+/// Part 1: Simple DFS with memoization
 fn dfs_part1(
-    machine: &mut [bool],
-    target_machine: &[bool],
+    lights: &mut [bool],
+    target_lights: &[bool],
     i: usize,
-    buttons: &[Vec<u64>],
+    buttons: &[Vec<usize>],
     cache: &mut HashMap<(Vec<bool>, usize), i64>,
 ) -> i64 {
-    if machine == target_machine {
+    if lights == target_lights {
         return 0;
     }
     if i == buttons.len() {
         return -1;
     }
 
-    if let Some(c) = cache.get(&(machine.to_vec(), i)) {
+    let key = (lights.to_vec(), i);
+    if let Some(c) = cache.get(&key) {
         return *c;
     }
 
     let mut result = i64::MAX;
     for j in i..buttons.len() {
-        for k in &buttons[j] {
-            machine[*k as usize] = !machine[*k as usize];
+        for &k in &buttons[j] {
+            lights[k] = !lights[k];
         }
-        let r = 1 + dfs_part1(machine, target_machine, j + 1, buttons, cache);
+        let r = 1 + dfs_part1(lights, target_lights, j + 1, buttons, cache);
         if r > 0 {
             result = result.min(r);
         }
-        for k in &buttons[j] {
-            machine[*k as usize] = !machine[*k as usize];
+        for &k in &buttons[j] {
+            lights[k] = !lights[k];
         }
     }
 
-    cache.insert((machine.to_vec(), i), result);
+    cache.insert(key, result);
 
     result
 }
 
-/// don't do this at home
-fn combinations(max: u64, c: u64) -> Vec<Vec<u64>> {
-    let mut result = Vec::new();
-    if c == 1 {
-        result.push(vec![max]);
-    } else if c == 2 {
-        for a in 0..=max {
-            result.push(vec![a, max - a]);
-        }
-    } else if c == 3 {
-        for a in 0..=max {
-            for b in 0..=max - a {
-                let c = max - a - b;
-                result.push(vec![a, b, c]);
-            }
-        }
-    } else if c == 4 {
-        for a in 0..=max {
-            for b in 0..=max - a {
-                for c in 0..=max - a - b {
-                    let d = max - a - b - c;
-                    result.push(vec![a, b, c, d]);
-                }
-            }
-        }
-    } else if c == 5 {
-        for a in 0..=max {
-            for b in 0..=max - a {
-                for c in 0..=max - a - b {
-                    for d in 0..=max - a - b - c {
-                        let e = max - a - b - c - d;
-                        result.push(vec![a, b, c, d, e]);
-                    }
-                }
-            }
-        }
-    } else if c == 6 {
-        for a in 0..=max {
-            for b in 0..=max - a {
-                for c in 0..=max - a - b {
-                    for d in 0..=max - a - b - c {
-                        for e in 0..=max - a - b - c - d {
-                            let f = max - a - b - c - d - e;
-                            result.push(vec![a, b, c, d, e, f]);
-                        }
-                    }
-                }
-            }
-        }
-    } else if c == 7 {
-        for a in 0..=max {
-            for b in 0..=max - a {
-                for c in 0..=max - a - b {
-                    for d in 0..=max - a - b - c {
-                        for e in 0..=max - a - b - c - d {
-                            for f in 0..=max - a - b - c - d - e {
-                                let g = max - a - b - c - d - e - f;
-                                result.push(vec![a, b, c, d, e, f, g]);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } else if c == 8 {
-        for a in 0..=max {
-            for b in 0..=max - a {
-                for c in 0..=max - a - b {
-                    for d in 0..=max - a - b - c {
-                        for e in 0..=max - a - b - c - d {
-                            for f in 0..=max - a - b - c - d - e {
-                                for g in 0..=max - a - b - c - d - e - f {
-                                    let h = max - a - b - c - d - e - f - g;
-                                    result.push(vec![a, b, c, d, e, f, g, h]);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } else {
-        panic!("not enough c {c}");
+/// Initialize a slice of length `m` with the contents `[0, 0, ..., n]` and then
+/// repeatedly call this function to obtain all possible combinations of `m`
+/// integers that sum to `n`. The function returns `false` if there is no other
+/// combination.
+///
+/// Example (m=3, n=4):
+/// ```text
+/// [0, 0, 4]
+/// [0, 1, 3]
+/// [0, 2, 2]
+/// [0, 3, 1]
+/// [0, 4, 0]
+/// [1, 0, 3]
+/// [1, 1, 2]
+/// [1, 2, 1]
+/// [1, 3, 0]
+/// [2, 0, 2]
+/// [2, 1, 1]
+/// [2, 2, 0]
+/// [3, 0, 1]
+/// [3, 1, 0]
+/// [4, 0, 0]
+/// ```
+fn next_combination(combinations: &mut [usize]) -> bool {
+    let i = combinations.iter().rposition(|&v| v != 0).unwrap();
+    if i == 0 {
+        return false;
     }
-    result
+    let v = combinations[i];
+    combinations[i - 1] += 1;
+    combinations[i] = 0;
+    combinations[combinations.len() - 1] = v - 1;
+    true
 }
 
-fn dfs_part2(joltage: &[u64], buttons: &[Vec<u64>]) -> u64 {
+/// Part 2: Optimized DFS that tries to prune as many branches as possible
+fn dfs_part2(joltage: &[usize], buttons: &[Vec<usize>]) -> usize {
     if joltage.iter().all(|j| *j == 0) {
         return 0;
     }
 
+    // Important optimization: Find the joltage value with the lowest number of
+    // combinations of buttons to try. This allows us to prune branches as early
+    // as possible.
     let mut min = usize::MAX;
     let mut mini = 0;
     for (i, &jolt) in joltage.iter().enumerate() {
         if jolt > 0 {
-            let n_matching_buttons = buttons.iter().filter(|b| b.contains(&(i as u64))).count();
+            let n_matching_buttons = buttons.iter().filter(|b| b.contains(&i)).count();
             if n_matching_buttons < min {
                 min = n_matching_buttons;
                 mini = i;
@@ -143,44 +103,58 @@ fn dfs_part2(joltage: &[u64], buttons: &[Vec<u64>]) -> u64 {
     }
     let min = joltage[mini];
 
+    // get the buttons that affect the joltage value at position `mini`
     let matching_buttons = buttons
         .iter()
-        .filter(|b| b.contains(&(mini as u64)))
+        .filter(|b| b.contains(&mini))
         .collect::<Vec<_>>();
 
-    let mut result = u64::MAX;
+    let mut result = usize::MAX;
 
     if !matching_buttons.is_empty() {
+        // get all other buttons that do not affect the joltage value at
+        // position `mini`
         let remaining_buttons = buttons
             .iter()
-            .filter(|b| !b.contains(&(mini as u64)))
+            .filter(|b| !b.contains(&mini))
             .cloned()
             .collect::<Vec<_>>();
 
-        for c in 1..=matching_buttons.len() {
-            let counts = combinations(min, c as u64);
-            let mut new_joltage = joltage.to_vec();
-            for cnts in &counts {
-                let mut good = true;
-                new_joltage.copy_from_slice(joltage);
-                'buttons: for (bi, cnt) in cnts.iter().enumerate() {
-                    for b in matching_buttons[bi] {
-                        if new_joltage[*b as usize] >= *cnt {
-                            new_joltage[*b as usize] -= cnt;
-                        } else {
-                            good = false;
-                            break 'buttons;
-                        }
-                    }
-                }
-                if !good {
+        // try all combinations of matching buttons
+        let mut new_joltage = joltage.to_vec();
+        let mut counts = vec![0; matching_buttons.len()];
+        counts[matching_buttons.len() - 1] = min;
+        loop {
+            // count down joltage values and make sure we don't press a button
+            // too often (i.e. that the number of button presses is not higher
+            // than any of the values to decrease)
+            let mut good = true;
+            new_joltage.copy_from_slice(joltage);
+            'buttons: for (bi, &cnt) in counts.iter().enumerate() {
+                if cnt == 0 {
                     continue;
                 }
+                for &b in matching_buttons[bi] {
+                    if new_joltage[b] >= cnt {
+                        new_joltage[b] -= cnt;
+                    } else {
+                        good = false;
+                        break 'buttons;
+                    }
+                }
+            }
 
+            if good {
+                // recurse with decreased joltage values and with remaining buttons
                 let r = dfs_part2(&new_joltage, &remaining_buttons);
-                if r != u64::MAX {
+                if r != usize::MAX {
                     result = result.min(min + r);
                 }
+            }
+
+            // try next combination
+            if !next_combination(&mut counts) {
+                break;
             }
         }
     }
@@ -190,61 +164,80 @@ fn dfs_part2(joltage: &[u64], buttons: &[Vec<u64>]) -> u64 {
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
-    let lines = input.lines().collect::<Vec<_>>();
 
-    // part 1
+    // parse input
+    let machines = input
+        .lines()
+        .map(|l| {
+            let parts = l.split(" ").collect::<Vec<_>>();
+            let target_lights = parts[0].as_bytes()[1..parts[0].len() - 1]
+                .iter()
+                .map(|b| *b == b'#')
+                .collect::<Vec<_>>();
+            let buttons = parts[1..parts.len() - 1]
+                .iter()
+                .map(|b| {
+                    b[1..b.len() - 1]
+                        .split(',')
+                        .map(|v| v.parse::<usize>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            let target_joltage = parts[parts.len() - 1][1..parts[parts.len() - 1].len() - 1]
+                .split(',')
+                .map(|v| v.parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
+
+            Machine {
+                target_lights,
+                buttons,
+                target_joltage,
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // part 1 - simple DFS with memoization
     let mut total1 = 0;
-    for l in &lines {
-        let parts = l.split(" ").collect::<Vec<_>>();
-        let target_machine = parts[0];
-        let target_machine = target_machine.as_bytes()[1..target_machine.len() - 1]
-            .iter()
-            .map(|b| *b == b'#')
-            .collect::<Vec<_>>();
-        let buttons = &parts[1..parts.len() - 1];
-        let buttons = buttons
-            .iter()
-            .map(|b| {
-                b[1..b.len() - 1]
-                    .split(',')
-                    .map(|v| v.parse::<u64>().unwrap())
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-
-        let mut machine = vec![false; target_machine.len()];
+    for m in &machines {
+        let mut lights = vec![false; m.target_lights.len()];
         total1 += dfs_part1(
-            &mut machine,
-            &target_machine,
+            &mut lights,
+            &m.target_lights,
             0,
-            &buttons,
+            &m.buttons,
             &mut HashMap::new(),
         );
     }
     println!("{total1}");
 
-    // part 2
+    // part 2 - optimized DFS that tries to prune as many branches as possible
     let mut total2 = 0;
-    for l in lines {
-        let parts = l.split(" ").collect::<Vec<_>>();
-        let buttons = &parts[1..parts.len() - 1];
-        let buttons = buttons
-            .iter()
-            .map(|b| {
-                b[1..b.len() - 1]
-                    .split(',')
-                    .map(|v| v.parse::<u64>().unwrap())
-                    .collect::<Vec<_>>()
-            })
-            .collect::<Vec<_>>();
-
-        let target_joltage = parts[parts.len() - 1];
-        let target_joltage = target_joltage[1..target_joltage.len() - 1]
-            .split(',')
-            .map(|v| v.parse::<u64>().unwrap())
-            .collect::<Vec<_>>();
-
-        total2 += dfs_part2(&target_joltage, &buttons);
+    for m in &machines {
+        total2 += dfs_part2(&m.target_joltage, &m.buttons);
     }
     println!("{total2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::next_combination;
+
+    #[test]
+    fn test_next_combination() {
+        let max = 4;
+        let mut comb = vec![0, 0, 0, max];
+        for a in 0..=max {
+            for b in 0..=max - a {
+                for c in 0..=max - a - b {
+                    let d = max - a - b - c;
+                    assert_eq!(vec![a, b, c, d], comb);
+                    if a == 4 {
+                        assert!(!next_combination(&mut comb));
+                    } else {
+                        assert!(next_combination(&mut comb));
+                    }
+                }
+            }
+        }
+    }
 }
