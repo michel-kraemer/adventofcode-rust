4c8c3f451dedff08323e13390cd44315bcceb19c
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-14 16:24:42 +0100

    2025: Optimize day 9

--- 2025/day09/src/main.rs
+++ 2025/day09/src/main.rs
@@ -1,3 +1,11 @@
+//! Optimized solution using coordinate compression and prefix sums. Works for
+//! all test cases I could find on Reddit:
+//! * https://www.reddit.com/r/adventofcode/comments/1pi5rqn/2025_day_9_part_2_check_your_solution_with_this/
+//! * https://www.reddit.com/r/adventofcode/comments/1piet8x/2025_day_9_part_2_a_simple_cursed_input/
+//! * https://www.reddit.com/r/adventofcode/comments/1pi36pq/2025_day_9_part_2_more_examples_to_soften_your/
+//! * https://www.reddit.com/r/adventofcode/comments/1pit2un/2029_day_9_part_2_i_solved_this_one_but_my_code/
+//! * https://www.reddit.com/r/adventofcode/comments/1piqgc2/2025_day_9_check_your_code_with_this_test_input/
+
 use std::{
     collections::{HashMap, VecDeque},
     fs,
@@ -22,7 +30,7 @@ fn compress(mut orig: Vec<usize>) -> HashMap<usize, usize> {
     orig.dedup();
 
     let mut result = HashMap::new();
-    let mut new = 0;
+    let mut new = 1; // starting at 1 makes it easier to calculate prefix sums later
     let mut last_orig = orig[0];
     for v in orig {
         if v - last_orig > 1 {
@@ -50,8 +58,8 @@ fn get_compressed(
 }
 
 /// Perform a flood fill at the given position
-fn fill(pos: (usize, usize), grid: &mut [bool], width: usize, height: usize) {
-    if grid[pos.1 * width + pos.0] {
+fn fill(pos: (usize, usize), grid: &mut [Vec<u64>], width: usize, height: usize) {
+    if grid[pos.1][pos.0] > 0 {
         // nothing to do
         return;
     }
@@ -66,9 +74,9 @@ fn fill(pos: (usize, usize), grid: &mut [bool], width: usize, height: usize) {
                 && (nx as usize) < width
                 && ny >= 0
                 && (ny as usize) < height
-                && !grid[ny as usize * width + nx as usize]
+                && grid[ny as usize][nx as usize] == 0
             {
-                grid[ny as usize * width + nx as usize] = true;
+                grid[ny as usize][nx as usize] = 1;
                 queue.push_back((nx as usize, ny as usize));
             }
         }
@@ -110,20 +118,20 @@ fn main() {
     let width = compressed_x.values().max().unwrap() + 1;
     let height = compressed_y.values().max().unwrap() + 1;
 
-    let mut grid = vec![false; width * height];
+    let mut grid = vec![vec![0; width]; height];
     for c in coords.windows(2) {
         let a = c[0].1;
         let b = c[1].1;
         if a.0 == b.0 {
             // vertical edge
-            for y in a.1.min(b.1)..=a.1.max(b.1) {
-                grid[y * width + a.0] = true;
+            for cell in grid.iter_mut().take(a.1.max(b.1) + 1).skip(a.1.min(b.1)) {
+                cell[a.0] = 1;
             }
         } else {
             // horizontal edge
             let minx = a.0.min(b.0);
             let maxx = a.0.max(b.0);
-            grid[(a.1 * width + minx)..=(a.1 * width + maxx)].fill(true);
+            grid[a.1][minx..=maxx].fill(1);
         }
     }
 
@@ -152,6 +160,14 @@ fn main() {
         }
     }
 
+    // Compute prefix sums. At a given position (x,y), this gives us the
+    // number of occupied cells in the rectangle [0,0,x,y].
+    for y in 1..height {
+        for x in 1..width {
+            grid[y][x] += grid[y][x - 1] + grid[y - 1][x] - grid[y - 1][x - 1];
+        }
+    }
+
     // iterate over all possible rectangles
     let mut max1 = 0;
     let mut max2 = 0;
@@ -167,35 +183,20 @@ fn main() {
             // (only do this if necessary, i.e. if the area is larger than the
             // largest one already found)
             if area > max2 {
-                let mut ok = true;
                 let minx = a.1.0.min(b.1.0);
                 let maxx = a.1.0.max(b.1.0);
                 let miny = a.1.1.min(b.1.1);
                 let maxy = a.1.1.max(b.1.1);
 
-                for y in miny..=maxy {
-                    // performance optimization: check vertical edges first
-                    if !grid[y * width + minx] || !grid[y * width + maxx] {
-                        ok = false;
-                        break;
-                    }
-                }
+                // calculate how many occupied cells we expect
+                let expected_area = ((maxx - minx + 1) * (maxy - miny + 1)) as u64;
 
-                if ok {
-                    for y in miny..=maxy {
-                        if grid
-                            .iter()
-                            .skip(y * width + minx)
-                            .take(maxx - minx + 1)
-                            .any(|b| !*b)
-                        {
-                            ok = false;
-                            break;
-                        }
-                    }
-                }
+                // get the number of actually occupied cells
+                let prefix_area = grid[maxy][maxx] - grid[miny - 1][maxx] - grid[maxy][minx - 1]
+                    + grid[miny - 1][minx - 1];
 
-                if ok {
+                if prefix_area == expected_area {
+                    // the rectangle is completely inside the polygon
                     max2 = max2.max(area);
                 }
             }
