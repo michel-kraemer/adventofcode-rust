4df56fcdf1c7a6fdc49c26ebd74b50ca802c0123
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-21 16:08:22 +0100

    2015: Optimize day 4

--- 2015/day04/src/main.rs
+++ 2015/day04/src/main.rs
@@ -1,21 +1,41 @@
-use std::fs;
+use std::{
+    fs,
+    sync::atomic::{AtomicI64, Ordering},
+    thread,
+};
 
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let input = input.trim();
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let input = input.trim();
+    let i = AtomicI64::new(0);
+    let min1 = AtomicI64::new(i64::MAX);
+    let min2 = AtomicI64::new(i64::MAX);
 
-        let mut i = 0;
-        loop {
-            let str = format!("{input}{i}");
-            let digest = md5::compute(str.as_bytes());
-            let digest = format!("{digest:x}");
-            if (part1 && digest.starts_with("00000")) || (!part1 && digest.starts_with("000000")) {
-                break;
-            }
-            i += 1;
-        }
+    thread::scope(|scope| {
+        let n_threads = thread::available_parallelism().unwrap().into();
+        (0..n_threads).for_each(|_| {
+            scope.spawn(|| {
+                let mut str = input.to_string();
+                loop {
+                    let i = i.fetch_add(1, Ordering::Relaxed);
+                    if min1.load(Ordering::Relaxed) < i && min2.load(Ordering::Relaxed) < i {
+                        break;
+                    }
+                    str.truncate(input.len());
+                    str.push_str(&format!("{i}"));
+                    let digest = md5::compute(str.as_bytes());
+                    if digest.0[0] == 0 && digest.0[1] == 0 {
+                        if digest.0[2] == 0 {
+                            min2.fetch_min(i, Ordering::Relaxed);
+                        } else if (digest.0[2] >> 4) == 0 {
+                            min1.fetch_min(i, Ordering::Relaxed);
+                        }
+                    }
+                }
+            });
+        });
+    });
 
-        println!("{i}");
-    }
+    println!("{}", min1.load(Ordering::Relaxed));
+    println!("{}", min2.load(Ordering::Relaxed));
 }
