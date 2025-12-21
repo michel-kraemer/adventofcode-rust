4e68cc0812da0eeff8b927e68fbab4b45106b8f6
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-21 18:16:57 +0100

    2015: Optimize day 7

--- 2015/day07/src/main.rs
+++ 2015/day07/src/main.rs
@@ -1,65 +1,98 @@
-use std::{collections::HashMap, fs};
+use std::fs;
 
-fn eval<'a>(
-    instructions: &HashMap<&'a str, &'a str>,
-    wire: &'a str,
-    cache: &mut HashMap<&'a str, u16>,
-) -> u16 {
-    if let Some(c) = cache.get(wire) {
-        return *c;
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Operand {
+    Literal(u16),
+    Register(usize),
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Instruction {
+    Literal(u16),
+    Move(usize),
+    And(Operand, Operand),
+    Or(Operand, Operand),
+    Not(Operand),
+    Lshift(Operand, usize),
+    Rshift(Operand, usize),
+    Unknown,
+}
+
+fn to_register(s: &str) -> usize {
+    let bytes = s.as_bytes();
+    let mut result = (bytes[0] - b'a' + 1) as usize;
+    if bytes.len() > 1 {
+        result *= 27;
+        result += (bytes[1] - b'a' + 1) as usize;
     }
+    result
+}
 
-    let r = wire.parse().unwrap_or_else(|_| {
-        let instr = instructions[wire];
-        if instr.contains("AND") {
-            let ops = instr.split(" ").collect::<Vec<_>>();
-            eval(instructions, ops[0], cache) & eval(instructions, ops[2], cache)
-        } else if instr.contains("OR") {
-            let ops = instr.split(" ").collect::<Vec<_>>();
-            eval(instructions, ops[0], cache) | eval(instructions, ops[2], cache)
-        } else if instr.starts_with("NOT") {
-            let ops = instr.split(" ").collect::<Vec<_>>();
-            !eval(instructions, ops[1], cache)
-        } else if instr.contains("LSHIFT") {
-            let ops = instr.split(" ").collect::<Vec<_>>();
-            eval(instructions, ops[0], cache) << ops[2].parse::<u16>().unwrap()
-        } else if instr.contains("RSHIFT") {
-            let ops = instr.split(" ").collect::<Vec<_>>();
-            eval(instructions, ops[0], cache) >> ops[2].parse::<u16>().unwrap()
-        } else {
-            eval(instructions, instr, cache)
-        }
-    });
+fn eval_operand(wires: &mut [Instruction], operand: Operand) -> u16 {
+    match operand {
+        Operand::Literal(l) => l,
+        Operand::Register(r) => eval_register(wires, r),
+    }
+}
 
-    cache.insert(wire, r);
-
+fn eval_register(wires: &mut [Instruction], register: usize) -> u16 {
+    let r = match wires[register] {
+        Instruction::Literal(a) => return a,
+        Instruction::Move(a) => eval_register(wires, a),
+        Instruction::And(a, b) => eval_operand(wires, a) & eval_operand(wires, b),
+        Instruction::Or(a, b) => eval_operand(wires, a) | eval_operand(wires, b),
+        Instruction::Not(a) => !eval_operand(wires, a),
+        Instruction::Lshift(a, i) => eval_operand(wires, a) << i,
+        Instruction::Rshift(a, i) => eval_operand(wires, a) >> i,
+        Instruction::Unknown => panic!("Invalid input"),
+    };
+    wires[register] = Instruction::Literal(r);
     r
 }
 
-fn run(b_override: Option<&str>) -> u16 {
-    let input = fs::read_to_string("input.txt").expect("Could not read file");
-
-    let mut cache: HashMap<&str, u16> = HashMap::new();
-    let mut instructions = HashMap::new();
-    for l in input.lines() {
-        let (instr, output) = l.split_once(" -> ").unwrap();
-        instructions.insert(output, instr);
+fn parse_operand(o: &str) -> Operand {
+    if let Ok(literal) = o.parse::<u16>() {
+        Operand::Literal(literal)
+    } else {
+        Operand::Register(to_register(o))
     }
-
-    if let Some(bo) = b_override {
-        instructions.insert("b", bo);
-    }
-
-    let r = eval(&instructions, "a", &mut cache);
-    println!("{r}");
-
-    r
 }
 
 fn main() {
+    let mut wires = vec![Instruction::Unknown; 27 * 27];
+
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    for l in input.lines() {
+        let (instr, output) = l.split_once(" -> ").unwrap();
+        let parts = instr.split_ascii_whitespace().collect::<Vec<_>>();
+        let instr = if parts.len() == 1 {
+            let op = parse_operand(parts[0]);
+            match op {
+                Operand::Literal(l) => Instruction::Literal(l),
+                Operand::Register(r) => Instruction::Move(r),
+            }
+        } else if parts.len() == 2 {
+            Instruction::Not(parse_operand(parts[1]))
+        } else if parts[1] == "AND" {
+            Instruction::And(parse_operand(parts[0]), parse_operand(parts[2]))
+        } else if parts[1] == "OR" {
+            Instruction::Or(parse_operand(parts[0]), parse_operand(parts[2]))
+        } else if parts[1] == "LSHIFT" {
+            Instruction::Lshift(parse_operand(parts[0]), parts[2].parse().unwrap())
+        } else {
+            Instruction::Rshift(parse_operand(parts[0]), parts[2].parse().unwrap())
+        };
+        let output = to_register(output);
+        wires[output] = instr;
+    }
+
     // part 1
-    let a = run(None);
+    let mut wires_part1 = wires.clone();
+    let a = eval_register(&mut wires_part1, to_register("a"));
+    println!("{a}");
 
     // part 2
-    run(Some(&a.to_string()));
+    wires[to_register("b")] = Instruction::Literal(a);
+    let a = eval_register(&mut wires, to_register("a"));
+    println!("{a}");
 }
