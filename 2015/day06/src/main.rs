e6aa1d9da8fb5e7f10ea382fc8f105f908826e5f
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-21 17:13:16 +0100

    2015: Optimize day 6

--- 2015/day06/src/main.rs
+++ 2015/day06/src/main.rs
@@ -1,15 +1,26 @@
 use std::fs;
 
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let input = input
+        .lines()
+        .map(|l| {
+            let mut p = l.split(" ");
+            let (turn_on, turn_off, from, to) = if p.next().unwrap() == "turn" {
+                let onoff = p.next().unwrap();
+                let from = p.next().unwrap();
+                let to = p.nth(1).unwrap();
+                if onoff == "on" {
+                    (true, false, from, to)
+                } else {
+                    (false, true, from, to)
+                }
+            } else {
+                let from = p.next().unwrap();
+                let to = p.nth(1).unwrap();
+                (false, false, from, to)
+            };
 
-        let mut grid = vec![vec![0; 1000]; 1000];
-
-        for l in input.lines() {
-            let p = l.split(" ").collect::<Vec<_>>();
-            let to = p[p.len() - 1];
-            let from = p[p.len() - 3];
             let to = to
                 .split_once(',')
                 .map(|(a, b)| (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
@@ -19,15 +30,23 @@ fn main() {
                 .map(|(a, b)| (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
                 .unwrap();
 
+            (turn_on, turn_off, from, to)
+        })
+        .collect::<Vec<_>>();
+
+    for part1 in [true, false] {
+        let mut grid = vec![vec![0; 1000]; 1000];
+
+        for &(turn_on, turn_off, from, to) in &input {
             for row in &mut grid[from.1..=to.1] {
                 for c in &mut row[from.0..=to.0] {
-                    if l.starts_with("turn on") {
+                    if turn_on {
                         if part1 {
                             *c = 1;
                         } else {
                             *c += 1;
                         }
-                    } else if l.starts_with("turn off") {
+                    } else if turn_off {
                         if part1 {
                             *c = 0;
                         } else if *c > 0 {
