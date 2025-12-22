d7a83b42a048fc5bf40895f4fa130ec8783038fa
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-22 19:52:09 +0100

    2015: Optimize day 12

--- 2015/day12/src/main.rs
+++ 2015/day12/src/main.rs
@@ -1,35 +1,67 @@
 use std::fs;
 
-use serde_json::Value;
-
-fn is_red(v: &Value) -> bool {
-    match v {
-        Value::String(s) => s == "red",
-        _ => false,
-    }
-}
-
-fn visit(v: &Value, part1: bool) -> i64 {
-    match v {
-        Value::Null => 0,
-        Value::Bool(_) => 0,
-        Value::Number(n) => n.as_i64().unwrap(),
-        Value::String(_) => 0,
-        Value::Array(children) => children.iter().map(|c| visit(c, part1)).sum(),
-        Value::Object(props) => {
-            if part1 || !props.iter().any(|(_, v)| is_red(v)) {
-                props.iter().map(|(_, v)| visit(v, part1)).sum()
-            } else {
-                0
-            }
-        }
-    }
-}
+use actson::{JsonEvent, JsonParser, feeder::SliceJsonFeeder};
 
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let v: Value = serde_json::from_str(&input).unwrap();
-        println!("{}", visit(&v, part1));
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+
+    let feeder = SliceJsonFeeder::new(input.as_bytes());
+    let mut parser = JsonParser::new(feeder);
+
+    let mut sum1 = vec![0]; // sums for part 1
+    let mut sum2 = vec![0]; // sums for part 2 
+    let mut has_red = vec![false]; // whether the current object has a value "red"
+    let mut parse_object = vec![]; // whether we parse an object or an array
+
+    while let Some(event) = parser.next_event().unwrap() {
+        match event {
+            JsonEvent::StartObject => {
+                sum1.push(0);
+                sum2.push(0);
+                has_red.push(false);
+                parse_object.push(true);
+            }
+
+            JsonEvent::EndObject => {
+                // part 1
+                let v1 = sum1.pop().unwrap();
+                *sum1.last_mut().unwrap() += v1;
+
+                // part 2 - only add sum to parent if the current object has no
+                // value "red"
+                let v2 = sum2.pop().unwrap();
+                let hr = has_red.pop().unwrap();
+                if !hr {
+                    *sum2.last_mut().unwrap() += v2;
+                }
+
+                parse_object.pop();
+            }
+
+            JsonEvent::StartArray => {
+                parse_object.push(false);
+            }
+
+            JsonEvent::EndArray => {
+                parse_object.pop();
+            }
+
+            JsonEvent::ValueString => {
+                if *parse_object.last().unwrap() && parser.current_str().unwrap() == "red" {
+                    *has_red.last_mut().unwrap() = true;
+                }
+            }
+
+            JsonEvent::ValueInt => {
+                let v = parser.current_int::<i64>().unwrap();
+                *sum1.last_mut().unwrap() += v;
+                *sum2.last_mut().unwrap() += v;
+            }
+
+            _ => {}
+        }
     }
+
+    println!("{}", sum1[0]);
+    println!("{}", sum2[0]);
 }
