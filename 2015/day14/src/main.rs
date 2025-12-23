ec58d128ad237c291387c0c8b58a2e82110ef23b
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-23 13:29:53 +0100

    2015: Optimize day 14

--- 2015/day14/src/main.rs
+++ 2015/day14/src/main.rs
@@ -1,45 +1,44 @@
 use std::fs;
 
+#[inline]
+fn dist(i: usize, r: (&str, usize, usize, usize)) -> usize {
+    let f = r.2 + r.3;
+    r.1 * ((i / f) * r.2 + (i % f).min(r.2))
+}
+
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
 
-        let reindeer = input
-            .lines()
-            .map(|l| {
-                let mut parts = l.split_ascii_whitespace();
-                let name = parts.next().unwrap();
-                let speed = parts.nth(2).unwrap().parse::<usize>().unwrap();
-                let seconds = parts.nth(2).unwrap().parse::<usize>().unwrap();
-                let rest = parts.nth(6).unwrap().parse::<usize>().unwrap();
-                (name, speed, seconds, rest)
-            })
-            .collect::<Vec<_>>();
+    let reindeer = input
+        .lines()
+        .map(|l| {
+            let mut parts = l.split_ascii_whitespace();
+            let name = parts.next().unwrap();
+            let speed = parts.nth(2).unwrap().parse::<usize>().unwrap();
+            let seconds = parts.nth(2).unwrap().parse::<usize>().unwrap();
+            let rest = parts.nth(6).unwrap().parse::<usize>().unwrap();
+            (name, speed, seconds, rest)
+        })
+        .collect::<Vec<_>>();
 
-        let mut distances = vec![0; reindeer.len()];
-        let mut scores = vec![0; reindeer.len()];
+    let mut distances = vec![0; reindeer.len()];
+    let mut scores = vec![0; reindeer.len()];
 
-        for i in 0..2503 {
-            for (j, r) in reindeer.iter().enumerate() {
-                let f = r.2 + r.3;
-                let running = (i % f) < r.2;
-                if running {
-                    distances[j] += r.1;
-                }
-            }
-            let max = distances.iter().max().unwrap();
-            for (j, _) in reindeer.iter().enumerate() {
-                if distances[j] == *max {
-                    scores[j] += 1;
-                }
-            }
+    let mut max = 0;
+    for i in 1..=2503 {
+        max = 0;
+        for (r, d) in reindeer.iter().zip(distances.iter_mut()) {
+            let v = dist(i, *r);
+            *d = v;
+            max = max.max(v);
         }
-
-        if part1 {
-            let lead = distances.iter().enumerate().max_by_key(|e| *e.1).unwrap();
-            println!("{}", lead.1);
-        } else {
-            println!("{}", scores.iter().max().unwrap());
+        for (d, s) in distances.iter().zip(scores.iter_mut()) {
+            if *d == max {
+                *s += 1;
+            }
         }
     }
+
+    println!("{max}");
+    println!("{}", scores.iter().max().unwrap());
 }
