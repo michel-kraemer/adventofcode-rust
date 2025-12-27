59217cf883387d47e17d3e75c79cdd3bcd857736
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-27 18:54:22 +0100

    2016: Optimize day 5

--- 2016/day05/src/main.rs
+++ 2016/day05/src/main.rs
@@ -1,44 +1,107 @@
-use std::fs;
+use std::{
+    fs,
+    sync::{
+        atomic::{AtomicBool, AtomicI64, Ordering},
+        mpsc,
+    },
+    thread,
+};
 
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use md5::Digest;
+
+const BLOCK_SIZE: i64 = 1000;
 
 fn main() {
-    let block_size = 10000;
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let input = input.trim();
 
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let input = input.trim();
+    let index = AtomicI64::new(0);
+    let stop = AtomicBool::new(false);
+    let (sender, receiver) = mpsc::channel();
 
-        let mut result = vec![' '; 8];
-        let mut i = 0;
-        let mut ci = 0;
-        while ci < 8 {
-            let block = (i..i + block_size)
-                .into_par_iter()
-                .map(|i| {
-                    let pw = format!("{}{}", input, i);
-                    let digest = md5::compute(pw);
-                    format!("{:x}", digest)
-                })
-                .filter(|h| h.starts_with("00000"))
-                .collect::<Vec<_>>();
-            i += block_size;
+    let mut password1 = String::new();
+    let mut password2 = [' '; 8];
 
-            for hex in block {
-                let c = hex.as_bytes()[5];
-                if part1 {
-                    result[ci] = c as char;
-                    ci += 1;
-                } else if c.is_ascii_digit() {
-                    let pos = c - b'0';
-                    if pos < 8 && result[pos as usize] == ' ' {
-                        result[pos as usize] = hex.as_bytes()[6] as char;
-                        ci += 1;
+    thread::scope(|scope| {
+        // start threads that process numbers in blocks and produce digests
+        // starting with 5 zeroes
+        let n_threads = thread::available_parallelism().unwrap().into();
+        (0..n_threads).for_each(|_| {
+            scope.spawn(|| {
+                let mut str = input.to_string();
+                loop {
+                    let stop = stop.load(Ordering::Relaxed);
+                    if stop {
+                        break;
+                    }
+
+                    // fetch next block
+                    let index = index.fetch_add(BLOCK_SIZE, Ordering::Relaxed);
+
+                    // process next block
+                    let mut result = Vec::new();
+                    for j in index..index + BLOCK_SIZE {
+                        str.truncate(input.len());
+                        str.push_str(&format!("{j}"));
+                        let digest = md5::compute(str.as_bytes());
+                        if digest.0[0] == 0 && digest.0[1] == 0 && (digest.0[2] >> 4) == 0 {
+                            // Digest starts with 5 zeroes. Add it to result.
+                            result.push((j, digest));
+                        }
+                    }
+
+                    // send results of this block to main thread
+                    sender.send((index, result)).unwrap();
+                }
+            });
+        });
+
+        // wait for blocks from threads
+        let mut queue: Vec<(i64, Vec<(i64, Digest)>)> = Vec::new();
+        let mut next = 0;
+        'outer: for msg in receiver.iter() {
+            // restore order
+            let j = queue.partition_point(|o| o.0 < msg.0);
+            queue.insert(j, msg);
+
+            // remove as many ordered items from the queue as possible
+            while !queue.is_empty() && queue[0].0 == next {
+                let r = queue.remove(0);
+
+                for (_, d) in r.1 {
+                    // get character at 6th position
+                    let c = d[2] & 0b1111;
+
+                    if password1.len() < 8 {
+                        // add character to password for part 1
+                        password1.push_str(&format!("{:x}", c));
+                    }
+
+                    if c < 8 && password2[c as usize] == ' ' {
+                        // add character to password for part 2
+                        let c2 = d[3] >> 4;
+                        if c2 < 10 {
+                            password2[c as usize] = (c2 + b'0') as char;
+                        } else {
+                            password2[c as usize] = (c2 - 10 + b'a') as char;
+                        }
+                    }
+
+                    if password1.len() == 8 && password2.iter().all(|c| *c != ' ') {
+                        // stop if we've got both passwords
+                        break 'outer;
                     }
                 }
+
+                // wait for the next item in order
+                next += BLOCK_SIZE;
             }
         }
 
-        println!("{}", String::from_iter(result));
-    }
+        // signal all threads to stop
+        stop.store(true, Ordering::Relaxed);
+    });
+
+    println!("{password1}");
+    println!("{}", password2.iter().collect::<String>());
 }
--- README.md
+++ README.md
@@ -131,7 +131,7 @@ All measurements were performed with my [cargo bench-aoc](./cargo-bench-aoc/) to
 <td><strong><a href="./2016/day02/src/main.rs">Day 2:</a></strong></td><td align="right">5µs</td>
 <td><strong><a href="./2016/day03/src/main.rs">Day 3:</a></strong></td><td align="right">54µs</td>
 <td><strong><a href="./2016/day04/src/main.rs">Day 4:</a></strong></td><td align="right">55µs</td>
-<td><strong><a href="./2016/day05/src/main.rs">Day 5:</a></strong></td><td align="right">...</td>
+<td><strong><a href="./2016/day05/src/main.rs">Day 5:</a></strong></td><td align="right">376ms</td>
 </tr>
 <tr>
 <td><strong><a href="./2016/day06/src/main.rs">Day 6:</a></strong></td><td align="right">...</td>
