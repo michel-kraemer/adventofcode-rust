45459fa7acd179f2ed00590cec9b4dbd202df793
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-27 07:25:27 +0100

    2016: Optimize day 4

--- 2016/day04/src/main.rs
+++ 2016/day04/src/main.rs
@@ -1,51 +1,67 @@
-use std::{collections::HashMap, fs};
+use std::fs;
+
+const NORTHPOLE_OBJECT_STORAGE: &[u8; 24] = b"northpole object storage";
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
-    let rooms = input
-        .lines()
-        .map(|l| {
-            let (s, hash) = l.split_once('[').unwrap();
-            let hash = &hash[0..hash.len() - 1];
-            let (room, id) = s.split_at(s.rfind('-').unwrap() + 1);
-            (
-                room.strip_suffix('-').unwrap(),
-                id.parse::<u64>().unwrap(),
-                hash,
-            )
-        })
-        .collect::<Vec<_>>();
 
-    let mut room_part2 = None;
+    let mut counts = [0; 26];
     let mut sum = 0;
-    for r in rooms {
-        let mut chars: HashMap<char, usize> = HashMap::new();
-        for c in r.0.chars() {
+    let mut object_storage_id = 0;
+    for l in input.lines() {
+        let (s, hash) = l.split_once('[').unwrap();
+        let hash = &hash[0..hash.len() - 1];
+        let (room, id) = s.split_at(s.rfind('-').unwrap());
+        let id = id[1..].parse::<u64>().unwrap();
+
+        // count character frequencies
+        let mut max = 0;
+        counts.fill(0);
+        for c in room.bytes() {
             if c.is_ascii_alphabetic() {
-                *chars.entry(c).or_default() += 1;
+                let e = &mut counts[(c - b'a') as usize];
+                *e += 1;
+                max = max.max(*e);
             }
-        }
-        let mut chars = chars.into_iter().collect::<Vec<_>>();
-        chars.sort_by(|a, b| b.1.cmp(&a.1).then(a.0.cmp(&b.0)));
-        let hash = String::from_iter(chars.into_iter().take(5).map(|c| c.0));
-        if hash == r.2 {
-            sum += r.1;
         }
 
-        let mut decoded = String::new();
-        for c in r.0.chars() {
-            if c == '-' {
-                decoded.push(' ');
-            } else {
-                let d = ((((c as u64 - b'a' as u64) + r.1) % 26) as u8 + b'a') as char;
-                decoded.push(d);
+        // part 1: validate hash
+        let mut hash_bytes = hash.bytes();
+        let mut prev_h = hash_bytes.next().unwrap();
+        let mut good = counts[(prev_h - b'a') as usize] == max; // check first char
+        if good {
+            for h in hash_bytes {
+                let c = counts[(h - b'a') as usize];
+                // there must be a count, it must be less than or equal to the
+                // previous count, and if it's equal, the character must be less
+                // than the previous one
+                if c == 0 || c > max || (c == max && h <= prev_h) {
+                    good = false;
+                    break;
+                }
+                max = c;
+                prev_h = h;
+            }
+            if good {
+                sum += id;
             }
         }
-        if decoded == "northpole object storage" {
-            room_part2 = Some(r.1);
+
+        // part 2: decrypt room name
+        if object_storage_id == 0
+            && room.bytes().zip(NORTHPOLE_OBJECT_STORAGE).all(|(r, e)| {
+                let re = if r == b'-' {
+                    b' '
+                } else {
+                    (((r - b'a') as u64 + id) % 26) as u8 + b'a'
+                };
+                *e == re
+            })
+        {
+            object_storage_id = id;
         }
     }
 
-    println!("{}", sum);
-    println!("{}", room_part2.unwrap());
+    println!("{sum}");
+    println!("{object_storage_id}");
 }
