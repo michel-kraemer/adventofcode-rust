859b60127a5445b39a332cb98605808db402cf0a
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-28 14:34:50 +0100

    2016: Optimize day 10

--- 2016/day10/src/main.rs
+++ 2016/day10/src/main.rs
@@ -1,93 +1,99 @@
-use std::{
-    cmp::max,
-    collections::{BTreeSet, VecDeque},
-    fs,
-};
+use std::fs;
 
-#[derive(Default, Clone)]
+#[derive(Default, PartialEq, Eq, Clone, Copy)]
+enum Input {
+    #[default]
+    Unknown,
+    Value(u64),
+    Bot(usize, bool),
+}
+
+#[derive(Default)]
 struct Bot {
-    inputs: BTreeSet<u64>,
-    low: usize,
-    low_bot: bool,
-    high: usize,
-    high_bot: bool,
+    inputs: [Input; 2],
 }
 
 fn ensure<T: Default>(v: &mut Vec<T>, len: usize) {
-    v.resize_with(max(v.len(), len), Default::default);
+    if v.len() < len {
+        v.resize_with(len, Default::default);
+    }
+}
+
+fn add_input(bot: &mut Bot, input: Input) {
+    if bot.inputs[0] == Input::Unknown {
+        bot.inputs[0] = input;
+    } else {
+        bot.inputs[1] = input;
+    }
+}
+
+fn eval(input: Input, bots: &mut [Bot]) -> u64 {
+    match input {
+        Input::Unknown => panic!("Unknown input"),
+        Input::Value(v) => v,
+        Input::Bot(bot, high) => {
+            let v1 = eval(bots[bot].inputs[0], bots);
+            bots[bot].inputs[0] = Input::Value(v1);
+            let v2 = eval(bots[bot].inputs[1], bots);
+            bots[bot].inputs[1] = Input::Value(v2);
+            if high {
+                v1.max(v2)
+            } else {
+                v1.min(v2)
+            }
+        }
+    }
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
 
     let mut bots: Vec<Bot> = Vec::new();
-
+    let mut outputs: Vec<Input> = Vec::new();
     for l in input.lines() {
-        if l.starts_with("value ") {
-            let p = l.split(' ').collect::<Vec<_>>();
-            let v = p[1].parse::<u64>().unwrap();
-            let bot = p.last().unwrap().parse::<usize>().unwrap();
+        if l.starts_with("value") {
+            let mut parts = l.split_ascii_whitespace();
+            let value = parts.nth(1).unwrap().parse::<u64>().unwrap();
+            let bot = parts.nth(3).unwrap().parse::<usize>().unwrap();
             ensure(&mut bots, bot + 1);
-            bots[bot].inputs.insert(v);
-        } else if l.starts_with("bot") {
-            let p = l.split(' ').collect::<Vec<_>>();
-            let bot = p[1].parse::<usize>().unwrap();
-            ensure(&mut bots, bot + 1);
-            bots[bot].low_bot = p[5] == "bot";
-            bots[bot].low = p[6].parse::<usize>().unwrap();
-            bots[bot].high_bot = p[10] == "bot";
-            bots[bot].high = p[11].parse::<usize>().unwrap();
+            add_input(&mut bots[bot], Input::Value(value));
+        } else {
+            let mut parts = l.split_ascii_whitespace();
+            let from = parts.nth(1).unwrap().parse::<usize>().unwrap();
+            let low_type = parts.nth(3).unwrap();
+            let low = parts.next().unwrap().parse::<usize>().unwrap();
+            let high_type = parts.nth(3).unwrap();
+            let high = parts.next().unwrap().parse::<usize>().unwrap();
+            if low_type == "bot" {
+                ensure(&mut bots, low + 1);
+                add_input(&mut bots[low], Input::Bot(from, false));
+            } else {
+                ensure(&mut outputs, low + 1);
+                outputs[low] = Input::Bot(from, false);
+            }
+            if high_type == "bot" {
+                ensure(&mut bots, high + 1);
+                add_input(&mut bots[high], Input::Bot(from, true));
+            } else {
+                ensure(&mut outputs, high + 1);
+                outputs[high] = Input::Bot(from, true);
+            }
         }
     }
 
-    let mut queue = VecDeque::new();
-    bots.iter()
-        .enumerate()
-        .filter(|(_, b)| b.inputs.len() == 2)
-        .for_each(|(i, _)| queue.push_back(i));
+    // part 2: compute product of output[0], output[1], and output[2]
+    let total2 =
+        eval(outputs[0], &mut bots) * eval(outputs[1], &mut bots) * eval(outputs[2], &mut bots);
 
-    let mut outputs: Vec<u64> = Vec::new();
-
-    while !queue.is_empty() {
-        let bid = queue.pop_front().unwrap();
-        let (low_bot, blow, low, high_bot, bhigh, high) = {
-            let b = &bots[bid];
-
-            let mut iter = b.inputs.iter();
-            let low = iter.next().unwrap();
-            let high = iter.next().unwrap();
-
-            if *low == 17 && *high == 61 {
-                // part 1
-                println!("{}", bid);
-            }
-
-            (b.low_bot, b.low, *low, b.high_bot, b.high, *high)
-        };
-
-        if low_bot {
-            ensure(&mut bots, blow + 1);
-            bots[blow].inputs.insert(low);
-            if bots[blow].inputs.len() == 2 {
-                queue.push_back(blow);
-            }
-        } else {
-            ensure(&mut outputs, blow + 1);
-            outputs[blow] = low;
-        }
-
-        if high_bot {
-            ensure(&mut bots, bhigh + 1);
-            bots[bhigh].inputs.insert(high);
-            if bots[bhigh].inputs.len() == 2 {
-                queue.push_back(bhigh);
-            }
-        } else {
-            ensure(&mut outputs, bhigh + 1);
-            outputs[bhigh] = high;
+    // part 1: look for bot that compares 61 and 17
+    for i in 0..bots.len() {
+        let v1 = eval(bots[i].inputs[0], &mut bots);
+        let v2 = eval(bots[i].inputs[1], &mut bots);
+        if (v1 == 61 && v2 == 17) || (v1 == 17 && v2 == 61) {
+            println!("{i}");
+            break;
         }
     }
 
-    // part 2
-    println!("{}", outputs[0] * outputs[1] * outputs[2]);
+    println!("{total2}");
 }
