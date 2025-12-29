579341457c86157890b939e4b7f2e15d52c87598
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-29 19:52:43 +0100

    2016: Optimize day 14

--- 2016/day14/src/main.rs
+++ 2016/day14/src/main.rs
@@ -1,74 +1,111 @@
 use std::{collections::VecDeque, fs};
 
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::prelude::*;
+
+fn hex_to_u8(c: u8) -> u8 {
+    if c.is_ascii_digit() {
+        c - b'0'
+    } else {
+        c - b'a' + 10
+    }
+}
 
 fn find_three(s: &[u8]) -> Option<u8> {
     for i in 0..s.len() - 2 {
         let c = s[i];
         if s[i + 1] == c && s[i + 2] == c {
-            return Some(c);
+            return Some(hex_to_u8(c));
         }
     }
     None
 }
 
-fn has_five(b: &[u8], l: u8) -> bool {
-    for i in 0..b.len() - 4 {
-        if b[i] == l && b[i + 1] == l && b[i + 2] == l && b[i + 3] == l && b[i + 4] == l {
-            return true;
+fn find_fives(s: &[u8]) -> u64 {
+    let mut result = 0;
+    let mut i = 0;
+    while i < s.len() - 5 {
+        let c = s[i];
+        if s[i + 1] == c && s[i + 2] == c && s[i + 3] == c && s[i + 4] == c {
+            result |= 1 << hex_to_u8(c);
+            i += 5;
+        } else {
+            i += 1;
         }
     }
-    false
+    result
 }
 
-fn find_last_index(salt: &str, part1: bool) -> usize {
-    let mut keys_found = 0;
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let salt = input.trim();
+
+    let mut keys_found1 = 0;
+    let mut keys_found2 = 0;
+
+    let mut result1 = 0;
+    let mut result2 = 0;
+
     let mut queue = VecDeque::new();
     let mut i = 0;
     loop {
         if queue.len() < 1001 {
             queue.extend(
-                (0..4000)
+                (0..1001)
                     .into_par_iter()
                     .map(|j| {
-                        let mut s = format!(
+                        let s1 = format!(
                             "{:x}",
                             md5::compute(format!("{}{}", salt, i + queue.len() + j))
                         );
-                        if !part1 {
-                            for _ in 0..2016 {
-                                s = format!("{:x}", md5::compute(s));
-                            }
+                        let mut s2 = s1.clone();
+                        for _ in 0..2016 {
+                            s2 = format!("{:x}", md5::compute(s2));
                         }
-                        s
+                        let s1_fives = find_fives(s1.as_bytes());
+                        let s2_fives = find_fives(s2.as_bytes());
+                        (s1, s1_fives, s2, s2_fives)
                     })
                     .collect::<Vec<_>>(),
             );
         }
 
-        let s = queue.pop_front().unwrap();
-        let b = s.as_bytes();
-        if let Some(t) = find_three(b) {
-            for o in queue.iter().take(1000) {
-                if has_five(o.as_bytes(), t) {
-                    keys_found += 1;
-                    if keys_found == 64 {
-                        return i;
-                    }
-                    break;
+        let (s1, _, s2, _) = queue.pop_front().unwrap();
+        if result1 == 0 {
+            let b = s1.as_bytes();
+            if let Some(t) = find_three(b)
+                && queue
+                    .iter()
+                    .take(1000)
+                    .any(|(_, fives, _, _)| *fives & (1 << t) > 0)
+            {
+                keys_found1 += 1;
+                if keys_found1 == 64 {
+                    result1 = i;
+                }
+            }
+        }
+        if result2 == 0 {
+            let b = s2.as_bytes();
+            if let Some(t) = find_three(b)
+                && queue
+                    .iter()
+                    .take(1000)
+                    .any(|(_, _, _, fives)| *fives & (1 << t) > 0)
+            {
+                keys_found2 += 1;
+                if keys_found2 == 64 {
+                    result2 = i;
                 }
             }
         }
 
+        if result1 > 0 && result2 > 0 {
+            break;
+        }
+
         i += 1;
     }
-}
 
-fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let salt = input.trim();
-        let last_index = find_last_index(salt, part1);
-        println!("{:?}", last_index);
-    }
+    println!("{result1}");
+    println!("{result2}");
 }
