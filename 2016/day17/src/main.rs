02561c63d9742dfedab4dde370c9fbfbf970fa86
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-30 19:08:17 +0100

    2016: Optimize day 17

--- 2016/day17/src/main.rs
+++ 2016/day17/src/main.rs
@@ -1,99 +1,71 @@
-use std::{
-    cmp::{Ordering, Reverse},
-    collections::BinaryHeap,
-    fs,
-};
+use std::{collections::VecDeque, fs};
 
-#[derive(PartialEq, Eq, Debug)]
-struct State {
-    x: i32,
-    y: i32,
-    path: String,
-}
-
-impl Ord for State {
-    fn cmp(&self, other: &State) -> Ordering {
-        self.path.len().cmp(&other.path.len())
-    }
-}
-
-impl PartialOrd for State {
-    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+use md5::Context;
 
 fn is_open(b: u8) -> bool {
     !b.is_ascii_digit() && b != b'a'
 }
 
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let passcode = input.trim();
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let passcode = input.trim();
 
-        let mut queue = BinaryHeap::new();
-        queue.push(Reverse(State {
-            x: 0,
-            y: 0,
-            path: passcode.to_string(),
-        }));
+    for part1 in [true, false] {
+        let mut queue = VecDeque::new();
+        let mut initial_context = Context::new();
+        initial_context.consume(passcode);
+        queue.push_back((0, 0, initial_context, "".to_string()));
 
         let mut max = 0;
-        while !queue.is_empty() {
-            let s = queue.pop().unwrap().0;
-
-            if s.x == 3 && s.y == 3 {
+        while let Some((x, y, context, path)) = queue.pop_front() {
+            if x == 3 && y == 3 {
                 if part1 {
-                    println!("{}", &s.path[passcode.len()..]);
+                    println!("{path}");
                     break;
                 } else {
-                    let steps = s.path.len() - passcode.len();
-                    if steps > max {
-                        max = steps;
-                    }
+                    max = max.max(path.len());
                     continue;
                 }
             }
 
-            let hash = format!("{:x}", md5::compute(&s.path));
+            let hash = format!("{:x}", context.clone().compute());
             let up = is_open(hash.as_bytes()[0]);
             let down = is_open(hash.as_bytes()[1]);
             let left = is_open(hash.as_bytes()[2]);
             let right = is_open(hash.as_bytes()[3]);
 
-            if up && s.y > 0 {
-                queue.push(Reverse(State {
-                    x: s.x,
-                    y: s.y - 1,
-                    path: format!("{}U", s.path),
-                }));
+            if up && y > 0 {
+                let mut new_path = path.clone();
+                new_path.push('U');
+                let mut new_context = context.clone();
+                new_context.consume(b"U");
+                queue.push_back((x, y - 1, new_context, new_path));
             }
-            if down && s.y < 3 {
-                queue.push(Reverse(State {
-                    x: s.x,
-                    y: s.y + 1,
-                    path: format!("{}D", s.path),
-                }));
+            if down && y < 3 {
+                let mut new_path = path.clone();
+                new_path.push('D');
+                let mut new_context = context.clone();
+                new_context.consume(b"D");
+                queue.push_back((x, y + 1, new_context, new_path));
             }
-            if left && s.x > 0 {
-                queue.push(Reverse(State {
-                    x: s.x - 1,
-                    y: s.y,
-                    path: format!("{}L", s.path),
-                }));
+            if left && x > 0 {
+                let mut new_path = path.clone();
+                new_path.push('L');
+                let mut new_context = context.clone();
+                new_context.consume(b"L");
+                queue.push_back((x - 1, y, new_context, new_path));
             }
-            if right && s.x < 3 {
-                queue.push(Reverse(State {
-                    x: s.x + 1,
-                    y: s.y,
-                    path: format!("{}R", s.path),
-                }));
+            if right && x < 3 {
+                let mut new_path = path.clone();
+                new_path.push('R');
+                let mut new_context = context.clone();
+                new_context.consume(b"R");
+                queue.push_back((x + 1, y, new_context, new_path));
             }
         }
 
         if !part1 {
-            println!("{}", max);
+            println!("{max}");
         }
     }
 }
