56f2f1f896bd84ac38fa2316bc7eb99286b34f4d
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2025-12-31 17:50:59 +0100

    2016: Optimize day 21

--- 2016/day21/src/main.rs
+++ 2016/day21/src/main.rs
@@ -1,95 +1,144 @@
 use std::fs;
 
-fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let mut instructions = input.lines().collect::<Vec<_>>();
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Instruction {
+    SwapPosition(usize, usize),
+    SwapLetter(u8, u8),
+    RotateLeft(usize),
+    RotateRight(usize),
+    RotateBasedOnLetter(u8),
+    ReversePositions(usize, usize),
+    MovePosition(usize, usize),
+}
 
-        let mut s = (if part1 { "abcdefgh" } else { "fbgdceah" })
-            .chars()
-            .collect::<Vec<_>>();
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let instructions = input
+        .lines()
+        .map(|l| {
+            if l.starts_with("swap position") {
+                let mut params = l.split_ascii_whitespace();
+                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
+                let y = params.next_back().unwrap().parse::<usize>().unwrap();
+                Instruction::SwapPosition(x, y)
+            } else if l.starts_with("swap letter") {
+                let mut params = l.split_ascii_whitespace();
+                let x = params.nth(2).unwrap().bytes().next().unwrap();
+                let y = params.next_back().unwrap().bytes().next().unwrap();
+                Instruction::SwapLetter(x, y)
+            } else if l.starts_with("rotate left") {
+                let mut params = l.split_ascii_whitespace();
+                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
+                Instruction::RotateLeft(x)
+            } else if l.starts_with("rotate right") {
+                let mut params = l.split_ascii_whitespace();
+                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
+                Instruction::RotateRight(x)
+            } else if l.starts_with("rotate based on position of letter") {
+                let mut params = l.split_ascii_whitespace();
+                let x = params.next_back().unwrap().bytes().next().unwrap();
+                Instruction::RotateBasedOnLetter(x)
+            } else if l.starts_with("reverse positions") {
+                let mut params = l.split_ascii_whitespace();
+                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
+                let y = params.next_back().unwrap().parse::<usize>().unwrap();
+                Instruction::ReversePositions(x, y)
+            } else if l.starts_with("move position") {
+                let mut params = l.split_ascii_whitespace();
+                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
+                let y = params.next_back().unwrap().parse::<usize>().unwrap();
+                Instruction::MovePosition(x, y)
+            } else {
+                panic!("Unknown instruction: {l}");
+            }
+        })
+        .collect::<Vec<_>>();
+
+    for part1 in [true, false] {
+        let mut instructions = instructions.clone();
+        let mut s = if part1 {
+            b"abcdefgh".to_vec()
+        } else {
+            b"fbgdceah".to_vec()
+        };
 
         if !part1 {
             instructions.reverse();
         }
 
         for i in instructions {
-            if i.starts_with("swap position") {
-                let mut params = i.split(' ');
-                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
-                let y = params.next_back().unwrap().parse::<usize>().unwrap();
-                s.swap(x, y);
-            } else if i.starts_with("swap letter") {
-                let mut params = i.split(' ');
-                let x = params.nth(2).unwrap().chars().next().unwrap();
-                let y = params.next_back().unwrap().chars().next().unwrap();
-                s = s
-                    .into_iter()
-                    .map(|c| if c == x { '#' } else { c })
-                    .map(|c| if c == y { x } else { c })
-                    .map(|c| if c == '#' { y } else { c })
-                    .collect();
-            } else if i.starts_with("rotate left") {
-                let mut params = i.split(' ');
-                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
-                if part1 {
-                    s.rotate_left(x);
-                } else {
-                    s.rotate_right(x);
+            match i {
+                Instruction::SwapPosition(x, y) => {
+                    s.swap(x, y);
                 }
-            } else if i.starts_with("rotate right") {
-                let mut params = i.split(' ');
-                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
-                if part1 {
-                    s.rotate_right(x);
-                } else {
-                    s.rotate_left(x);
+                Instruction::SwapLetter(x, y) => {
+                    s = s
+                        .into_iter()
+                        .map(|c| {
+                            if c == x {
+                                b'#'
+                            } else if c == y {
+                                x
+                            } else {
+                                c
+                            }
+                        })
+                        .map(|c| if c == b'#' { y } else { c })
+                        .collect();
                 }
-            } else if i.starts_with("rotate based on position of letter") {
-                let mut params = i.split(' ');
-                let x = params.next_back().unwrap().chars().next().unwrap();
-
-                if part1 {
-                    let idx = s.iter().position(|c| *c == x).unwrap();
-                    let len = s.len();
-                    s.rotate_right((1 + idx + (if idx >= 4 { 1 } else { 0 })) % len);
-                } else {
-                    let mut cl = s.clone();
-                    loop {
-                        cl.rotate_left(1);
-                        let mut cl2 = cl.clone();
-                        let idx = cl2.iter().position(|c| *c == x).unwrap();
-                        let len = cl2.len();
-                        cl2.rotate_right((1 + idx + (if idx >= 4 { 1 } else { 0 })) % len);
-                        if cl2 == s {
-                            s = cl;
-                            break;
+                Instruction::RotateLeft(x) => {
+                    if part1 {
+                        s.rotate_left(x);
+                    } else {
+                        s.rotate_right(x);
+                    }
+                }
+                Instruction::RotateRight(x) => {
+                    if part1 {
+                        s.rotate_right(x);
+                    } else {
+                        s.rotate_left(x);
+                    }
+                }
+                Instruction::RotateBasedOnLetter(x) => {
+                    if part1 {
+                        let idx = s.iter().position(|c| *c == x).unwrap();
+                        let len = s.len();
+                        s.rotate_right((1 + idx + (if idx >= 4 { 1 } else { 0 })) % len);
+                    } else {
+                        let mut cl = s.clone();
+                        loop {
+                            cl.rotate_left(1);
+                            let mut cl2 = cl.clone();
+                            let idx = cl2.iter().position(|c| *c == x).unwrap();
+                            let len = cl2.len();
+                            cl2.rotate_right((1 + idx + (if idx >= 4 { 1 } else { 0 })) % len);
+                            if cl2 == s {
+                                s = cl;
+                                break;
+                            }
                         }
                     }
                 }
-            } else if i.starts_with("reverse positions") {
-                let mut params = i.split(' ');
-                let mut x = params.nth(2).unwrap().parse::<usize>().unwrap();
-                let mut y = params.next_back().unwrap().parse::<usize>().unwrap();
-                while x < y {
-                    s.swap(x, y);
-                    x += 1;
-                    y -= 1;
+                Instruction::ReversePositions(mut x, mut y) => {
+                    while x < y {
+                        s.swap(x, y);
+                        x += 1;
+                        y -= 1;
+                    }
                 }
-            } else if i.starts_with("move position") {
-                let mut params = i.split(' ');
-                let x = params.nth(2).unwrap().parse::<usize>().unwrap();
-                let y = params.next_back().unwrap().parse::<usize>().unwrap();
-                if part1 {
-                    let c = s.remove(x);
-                    s.insert(y, c);
-                } else {
-                    let c = s.remove(y);
-                    s.insert(x, c);
+                Instruction::MovePosition(x, y) => {
+                    if part1 {
+                        let c = s.remove(x);
+                        s.insert(y, c);
+                    } else {
+                        let c = s.remove(y);
+                        s.insert(x, c);
+                    }
                 }
             }
         }
 
-        println!("{}", String::from_iter(s));
+        println!("{}", s.into_iter().map(|b| b as char).collect::<String>());
     }
 }
--- README.md
+++ README.md
@@ -159,7 +159,7 @@ All measurements were performed with my [cargo bench-aoc](./cargo-bench-aoc/) to
 <td><strong><a href="./2016/day20/src/main.rs">Day 20:</a></strong></td><td align="right">37µs</td>
 </tr>
 <tr>
-<td><strong><a href="./2016/day21/src/main.rs">Day 21:</a></strong></td><td align="right">...</td>
+<td><strong><a href="./2016/day21/src/main.rs">Day 21:</a></strong></td><td align="right">7µs</td>
 <td><strong><a href="./2016/day22/src/main.rs">Day 22:</a></strong></td><td align="right">...</td>
 <td><strong><a href="./2016/day23/src/main.rs">Day 23:</a></strong></td><td align="right">...</td>
 <td><strong><a href="./2016/day24/src/main.rs">Day 24:</a></strong></td><td align="right">...</td>
