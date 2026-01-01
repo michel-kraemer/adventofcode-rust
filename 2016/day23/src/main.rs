e5eb7f9375380dc4953215b7062484e8bfc7c1a8
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-01 14:53:31 +0100

    2016: Optimize day 23

--- 2016/day23/src/main.rs
+++ 2016/day23/src/main.rs
@@ -1,49 +1,95 @@
-use std::fs;
+use std::{
+    fmt::{Display, Formatter},
+    fs,
+};
 
-#[derive(PartialEq, Eq, Debug)]
-enum Instruction {
-    Cpy,
-    Inc,
-    Dec,
-    Jnz,
-    Tgl,
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Input {
+    Value(i64),
+    Register(usize),
 }
 
-enum Param {
-    Reg(usize),
-    Num(i32),
+impl Display for Input {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Input::Value(v) => write!(f, "{v}"),
+            Input::Register(r) => write!(f, "{}", (b'a' + (*r as u8)) as char),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Instruction {
+    Cpy { input: Input, register: Input },
+    Inc { register: Input, offset: i64 },
+    Jnz { input: Input, offset: Input },
+    Tgl { input: Input },
+}
+
+impl Display for Instruction {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Instruction::Cpy { input, register } => write!(f, "cpy {input} {register}"),
+            Instruction::Inc { register, offset } => {
+                if *offset == 1 {
+                    write!(f, "inc {register}")
+                } else if *offset == -1 {
+                    write!(f, "dec {register}")
+                } else {
+                    write!(f, "inc {register} {offset}")
+                }
+            }
+            Instruction::Jnz { input, offset } => write!(f, "jnz {input} {offset}"),
+            Instruction::Tgl { input } => write!(f, "tgl {input}"),
+        }
+    }
+}
+
+fn parse_input(s: &str) -> Input {
+    match s {
+        "a" => Input::Register(0),
+        "b" => Input::Register(1),
+        "c" => Input::Register(2),
+        "d" => Input::Register(3),
+        _ => Input::Value(s.parse().unwrap()),
+    }
 }
 
 fn main() {
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let instructions = input
+        .lines()
+        .map(|l| {
+            let mut parts = l.split_ascii_whitespace();
+            let instruction = parts.next().unwrap();
+            match instruction {
+                "cpy" => Instruction::Cpy {
+                    input: parse_input(parts.next().unwrap()),
+                    register: parse_input(parts.next().unwrap()),
+                },
+                "inc" => Instruction::Inc {
+                    register: parse_input(parts.next().unwrap()),
+                    offset: 1,
+                },
+                "dec" => Instruction::Inc {
+                    register: parse_input(parts.next().unwrap()),
+                    offset: -1,
+                },
+                "jnz" => Instruction::Jnz {
+                    input: parse_input(parts.next().unwrap()),
+                    offset: parse_input(parts.next().unwrap()),
+                },
+                "tgl" => Instruction::Tgl {
+                    input: parse_input(parts.next().unwrap()),
+                },
+                _ => panic!("Unknown instruction: {instruction}"),
+            }
+        })
+        .collect::<Vec<_>>();
+
     for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-
-        let mut instructions = input
-            .lines()
-            .map(|l| {
-                let mut parts = l.split(' ');
-                let i = match parts.next().unwrap() {
-                    "cpy" => Instruction::Cpy,
-                    "inc" => Instruction::Inc,
-                    "dec" => Instruction::Dec,
-                    "jnz" => Instruction::Jnz,
-                    "tgl" => Instruction::Tgl,
-                    _ => unreachable!(),
-                };
-                let params = parts
-                    .map(|p| {
-                        if let Ok(n) = p.parse::<i32>() {
-                            Param::Num(n)
-                        } else {
-                            Param::Reg((p.as_bytes()[0] - b'a') as usize)
-                        }
-                    })
-                    .collect::<Vec<_>>();
-                (i, params)
-            })
-            .collect::<Vec<_>>();
-
-        let mut registers = [0i32; 26];
+        let mut instructions = instructions.clone();
+        let mut registers: [i64; 4] = [0; 4];
 
         if part1 {
             registers[0] = 7;
@@ -51,100 +97,146 @@ fn main() {
             registers[0] = 12;
         }
 
-        let mut pointer = 0;
-        while pointer < instructions.len() {
-            let (i, params) = &instructions[pointer];
-            match i {
-                Instruction::Cpy => {
-                    let v = match params[0] {
-                        Param::Num(n) => n,
-                        Param::Reg(r) => registers[r],
-                    };
-                    if let Param::Reg(r) = params[1] {
-                        registers[r] = v;
+        let mut ip = 0;
+        while ip < instructions.len() {
+            // println!("EXEC {:?}", instructions[ip]);
+            match instructions[ip] {
+                Instruction::Cpy { input, register } => {
+                    if let Input::Register(register) = register {
+                        match input {
+                            Input::Value(v) => registers[register] = v,
+                            Input::Register(r) => registers[register] = registers[r],
+                        }
                     }
+                    ip += 1;
                 }
-                Instruction::Inc => {
-                    let Param::Reg(r) = params[0] else {
-                        unreachable!()
-                    };
-                    registers[r] += 1;
+
+                Instruction::Inc { register, offset } => {
+                    if let Input::Register(register) = register {
+                        registers[register] += offset;
+                    }
+                    ip += 1;
                 }
-                Instruction::Dec => {
-                    let Param::Reg(r) = params[0] else {
-                        unreachable!()
-                    };
-                    registers[r] -= 1;
-                }
-                Instruction::Jnz => {
-                    let v = match params[0] {
-                        Param::Num(n) => n,
-                        Param::Reg(r) => registers[r],
+
+                Instruction::Jnz { input, offset } => {
+                    let v = match input {
+                        Input::Value(v) => v,
+                        Input::Register(r) => registers[r],
                     };
                     if v != 0 {
-                        let d = match params[1] {
-                            Param::Num(n) => n,
-                            Param::Reg(r) => registers[r],
-                        };
-                        if d == -2
-                            && pointer > 1
-                            && instructions[pointer - 2].0 == Instruction::Inc
-                            && instructions[pointer - 1].0 == Instruction::Dec
+                        let offset = match offset {
+                            Input::Value(value) => value,
+                            Input::Register(register) => registers[register],
+                        } as isize;
+
+                        // JIT-optimization:
+                        if offset == -2
+                            && ip > 1
+                            && let Instruction::Inc {
+                                register: Input::Register(r1),
+                                offset: inc_offset1,
+                            } = instructions[ip - 2]
+                            && let Instruction::Inc {
+                                register: Input::Register(r2),
+                                offset: inc_offset2,
+                            } = instructions[ip - 1]
                         {
-                            let Param::Reg(r2) = instructions[pointer - 2].1[0] else {
-                                unreachable!()
-                            };
-                            let Param::Reg(r1) = instructions[pointer - 1].1[0] else {
-                                unreachable!()
-                            };
-                            registers[r2] += v;
-                            registers[r1] -= v;
-                            pointer += 1;
-                        } else if d == -2
-                            && pointer > 1
-                            && instructions[pointer - 2].0 == Instruction::Dec
-                            && instructions[pointer - 1].0 == Instruction::Inc
+                            // quick inc
+                            registers[r1] += v * inc_offset1;
+                            registers[r2] += v * inc_offset2;
+                            ip += 1;
+                        } else if offset == -5
+                            && ip > 4
+                            // first instruction must be a cpy
+                            && let Instruction::Cpy {
+                                input: cpy_input,
+                                register: Input::Register(cpy_dest),
+                            } = instructions[ip - 5]
+                            // second and third instructions must be inc/dec
+                            && let Instruction::Inc {
+                                register: Input::Register(inc1_r),
+                                offset: inc_offset1,
+                            } = instructions[ip - 4]
+                            && let Instruction::Inc {
+                                register: Input::Register(inc2_r),
+                                offset: inc_offset2,
+                            } = instructions[ip - 3]
+                            // one of these two must be an inc and the other
+                            // must be a dec
+                            && inc_offset1 == -inc_offset2
+                            // fourth instruction must be a jnz
+                            && let Instruction::Jnz {
+                                input: Input::Register(jnz_r),
+                                offset: Input::Value(jnz_v),
+                            } = instructions[ip - 2]
+                            // this jnz must use the register that is decremented
+                            && ((inc_offset1 == -1 && jnz_r == inc1_r)
+                                || (inc_offset2 == -1 && jnz_r == inc2_r))
+                            // and the jnz must have an offset of -2
+                            && jnz_v == -2
+                            // fifth instruction must be a dec
+                            && let Instruction::Inc {
+                                register: Input::Register(dec2_r),
+                                offset: inc_offset3,
+                            } = instructions[ip - 1]
+                            && inc_offset3 == -1
+                            // and this dec must affect our input
+                            && let Input::Register(my_r) = input
+                            && dec2_r == my_r
                         {
-                            let Param::Reg(r2) = instructions[pointer - 2].1[0] else {
-                                unreachable!()
+                            // quick multiplication as hinted in the problem
+                            // statement
+                            let cpy_source = match cpy_input {
+                                Input::Value(v) => v,
+                                Input::Register(r) => registers[r],
                             };
-                            let Param::Reg(r1) = instructions[pointer - 1].1[0] else {
-                                unreachable!()
-                            };
-                            registers[r2] -= v;
-                            registers[r1] += v;
-                            pointer += 1;
+                            if inc_offset1 == -1 {
+                                registers[inc2_r] += cpy_source * registers[my_r];
+                            } else {
+                                registers[inc1_r] += cpy_source * registers[my_r];
+                            }
+                            registers[cpy_dest] = 0;
+                            registers[my_r] = 0;
+                            ip += 1;
                         } else {
-                            pointer = (pointer as i32 + d) as usize;
+                            // execute the instruction normally
+                            ip = ip.checked_add_signed(offset).unwrap();
                         }
-                        continue;
+                    } else {
+                        ip += 1;
                     }
                 }
-                Instruction::Tgl => {
-                    let v = match params[0] {
-                        Param::Num(n) => n,
-                        Param::Reg(r) => registers[r],
-                    };
-                    let j = (pointer as i32 + v) as usize;
+
+                Instruction::Tgl { input } => {
+                    let j = ip
+                        + match input {
+                            Input::Value(value) => value,
+                            Input::Register(register) => registers[register],
+                        } as usize;
                     if j < instructions.len() {
-                        let target_instr = &instructions[j].0;
-                        let target_params = &instructions[j].1;
-                        if target_params.len() == 1 {
-                            if *target_instr == Instruction::Inc {
-                                instructions[j].0 = Instruction::Dec;
-                            } else {
-                                instructions[j].0 = Instruction::Inc;
-                            }
-                        } else if *target_instr == Instruction::Jnz {
-                            instructions[j].0 = Instruction::Cpy;
-                        } else {
-                            instructions[j].0 = Instruction::Jnz;
-                        }
+                        let target_instr = &mut instructions[j];
+                        *target_instr = match target_instr {
+                            Instruction::Cpy { input, register } => Instruction::Jnz {
+                                input: *input,
+                                offset: *register,
+                            },
+                            Instruction::Inc { register, offset } => Instruction::Inc {
+                                register: *register,
+                                offset: -*offset,
+                            },
+                            Instruction::Jnz { input, offset } => Instruction::Cpy {
+                                input: *input,
+                                register: *offset,
+                            },
+                            Instruction::Tgl { input } => Instruction::Inc {
+                                register: *input,
+                                offset: 1,
+                            },
+                        };
                     }
+                    ip += 1;
                 }
             }
-
-            pointer += 1;
         }
 
         println!("{}", registers[0]);
--- README.md
+++ README.md
@@ -167,7 +167,7 @@ All measurements were performed with my [cargo bench-aoc](./cargo-bench-aoc/) to
 <tr>
 <td><strong><a href="./2016/day21/src/main.rs">Day 21:</a></strong></td><td align="right">7µs</td>
 <td><strong><a href="./2016/day22/src/main.rs">Day 22:</a></strong></td><td align="right">3ms</td>
-<td><strong><a href="./2016/day23/src/main.rs">Day 23:</a></strong></td><td align="right">...</td>
+<td><strong><a href="./2016/day23/src/main.rs">Day 23:</a></strong></td><td align="right">1µs</td>
 <td><strong><a href="./2016/day24/src/main.rs">Day 24:</a></strong></td><td align="right">...</td>
 <td><strong><a href="./2016/day25/src/main.rs">Day 25:</a></strong></td><td align="right">...</td>
 </tr>
