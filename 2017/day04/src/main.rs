dfa26c8857c06b94ff4c67c0f9a37e7419b584a5
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-02 23:16:42 +0100

    2017: Optimize day 4

--- 2017/day04/src/main.rs
+++ 2017/day04/src/main.rs
@@ -1,29 +1,34 @@
 use std::fs;
 
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
 
-        let mut valid = 0;
-        for l in input.lines() {
+    // part 1
+    let valid1 = input
+        .lines()
+        .filter_map(|l| {
             let mut p = l
-                .split_whitespace()
-                .map(|w| {
-                    let mut c = w.chars().collect::<Vec<_>>();
-                    if !part1 {
-                        c.sort();
-                    }
-                    String::from_iter(c.into_iter())
-                })
+                .split_ascii_whitespace()
+                .map(|w| w.bytes().collect::<Vec<_>>())
                 .collect::<Vec<_>>();
             let len = p.len();
-            p.sort();
+            p.sort_unstable();
             p.dedup();
-            if len == p.len() {
-                valid += 1;
-            }
-        }
+            if len == p.len() { Some(p) } else { None }
+        })
+        .collect::<Vec<_>>();
+    println!("{}", valid1.len());
 
-        println!("{}", valid);
-    }
+    // part 2
+    let total2 = valid1
+        .into_iter()
+        .filter_map(|mut p| {
+            p.iter_mut().for_each(|w| w.sort_unstable());
+            let len = p.len();
+            p.sort_unstable();
+            p.dedup();
+            if len == p.len() { Some(true) } else { None }
+        })
+        .count();
+    println!("{total2}");
 }
