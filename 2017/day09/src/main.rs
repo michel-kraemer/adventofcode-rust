f337e00ffdb3ab03762baa2c4f74594dc6f40246
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-03 17:04:08 +0100

    2017: Optimize day 9

--- 2017/day09/src/main.rs
+++ 2017/day09/src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
-    let input = input.trim().chars().collect::<Vec<_>>();
+    let input = input.trim().bytes().collect::<Vec<_>>();
 
     let mut score = 0;
     let mut non_canceled = 0;
@@ -11,15 +11,15 @@ fn main() {
     while i < input.len() {
         let mut c = input[i];
         match c {
-            '{' => level += 1,
-            '}' => {
+            b'{' => level += 1,
+            b'}' => {
                 score += level;
                 level -= 1;
             }
-            '<' => {
-                while i < input.len() && c != '>' {
+            b'<' => {
+                while i < input.len() && c != b'>' {
                     c = input[i];
-                    if c == '!' {
+                    if c == b'!' {
                         i += 1;
                     } else {
                         non_canceled += 1;
@@ -34,6 +34,6 @@ fn main() {
         i += 1;
     }
 
-    println!("{}", score);
-    println!("{}", non_canceled);
+    println!("{score}");
+    println!("{non_canceled}");
 }
