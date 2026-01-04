7159dc3ff65e63cf3c7ba4fd3f14761ea1f78350
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-04 08:56:37 +0100

    2017: Optimize day 13

--- 2017/day13/src/main.rs
+++ 2017/day13/src/main.rs
@@ -1,43 +1,37 @@
 use std::fs;
 
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let layers = input
+        .lines()
+        .map(|l| {
+            let (layer, len) = l.split_once(": ").unwrap();
+            (
+                layer.parse::<usize>().unwrap(),
+                len.parse::<usize>().unwrap(),
+            )
+        })
+        .collect::<Vec<_>>();
 
-        let layers = input
-            .lines()
-            .map(|l| {
-                let (layer, len) = l.split_once(": ").unwrap();
-                (
-                    layer.parse::<usize>().unwrap(),
-                    len.parse::<usize>().unwrap(),
-                )
-            })
-            .collect::<Vec<_>>();
-
-        let mut delay = 0;
-        loop {
-            let mut severity = 0;
-            for l in &layers {
-                if (delay + l.0) % (l.1 * 2 - 2) == 0 {
-                    if part1 {
-                        severity += l.0 * l.1;
-                    } else {
-                        severity += 1;
-                        break;
-                    }
-                }
-            }
-
-            if part1 {
-                println!("{}", severity);
-                break;
-            } else if severity == 0 {
-                println!("{}", delay);
-                break;
-            }
-
-            delay += 1;
+    // part 1
+    let mut severity = 0;
+    for l in &layers {
+        if l.0 % (l.1 * 2 - 2) == 0 {
+            severity += l.0 * l.1;
         }
     }
+    println!("{severity}");
+
+    // part 2
+    let mut delay = 0;
+    'outer: loop {
+        delay += 1;
+        for l in &layers {
+            if (delay + l.0) % (l.1 * 2 - 2) == 0 {
+                continue 'outer;
+            }
+        }
+        break;
+    }
+    println!("{delay}");
 }
