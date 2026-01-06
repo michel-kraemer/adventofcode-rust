cc41a92bbb3e1445d77f3a7e4e2c0e4ae88d620c
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-06 07:06:58 +0100

    2017: Optimize day 18

--- 2017/day18/src/main.rs
+++ 2017/day18/src/main.rs
@@ -18,7 +18,7 @@ enum Value {
 struct Program<'a> {
     instructions: &'a Vec<Instruction>,
     pointer: i64,
-    registers: Vec<i64>,
+    registers: [i64; 26],
     sent_messages: usize,
 }
 
@@ -27,7 +27,7 @@ impl<'a> Program<'a> {
         let mut r = Program {
             instructions,
             pointer: 0,
-            registers: vec![0; 26],
+            registers: [0; 26],
             sent_messages: 0,
         };
         r.registers[to_reg("p")] = initial_p;
@@ -92,56 +92,49 @@ fn to_reg(s: &str) -> usize {
     (s.as_bytes()[0] - b'a') as usize
 }
 
+fn to_value(s: &str) -> Value {
+    if let Ok(v) = s.parse::<i64>() {
+        Value::Const(v)
+    } else {
+        Value::Reg(to_reg(s))
+    }
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
     let instructions = input
         .lines()
         .map(|l| {
-            let p = l.split(' ').collect::<Vec<_>>();
-            let p1 = if let Ok(v) = p[1].parse::<i64>() {
-                Value::Const(v)
-            } else {
-                Value::Reg(to_reg(p[1]))
-            };
-            let p2 = if p.len() > 2 {
-                Some(if let Ok(v) = p[2].parse::<i64>() {
-                    Value::Const(v)
-                } else {
-                    Value::Reg(to_reg(p[2]))
-                })
-            } else {
-                None
-            };
-            match p[0] {
-                "snd" => Instruction::Snd(p1),
-                "set" => Instruction::Set(to_reg(p[1]), p2.unwrap()),
-                "add" => Instruction::Add(to_reg(p[1]), p2.unwrap()),
-                "mul" => Instruction::Mul(to_reg(p[1]), p2.unwrap()),
-                "mod" => Instruction::Mod(to_reg(p[1]), p2.unwrap()),
-                "rcv" => Instruction::Rcv(to_reg(p[1])),
-                "jgz" => Instruction::Jgz(p1, p2.unwrap()),
+            let mut p = l.split_ascii_whitespace();
+            match p.next().unwrap() {
+                "snd" => Instruction::Snd(to_value(p.next().unwrap())),
+                "set" => Instruction::Set(to_reg(p.next().unwrap()), to_value(p.next().unwrap())),
+                "add" => Instruction::Add(to_reg(p.next().unwrap()), to_value(p.next().unwrap())),
+                "mul" => Instruction::Mul(to_reg(p.next().unwrap()), to_value(p.next().unwrap())),
+                "mod" => Instruction::Mod(to_reg(p.next().unwrap()), to_value(p.next().unwrap())),
+                "rcv" => Instruction::Rcv(to_reg(p.next().unwrap())),
+                "jgz" => Instruction::Jgz(to_value(p.next().unwrap()), to_value(p.next().unwrap())),
                 _ => panic!(),
             }
         })
         .collect::<Vec<_>>();
 
     // part 1
-    let mut p = Program::new(&instructions, 0);
-    let sounds = p.run(VecDeque::new());
-    println!("{}", sounds.into_iter().last().unwrap());
+    let mut q = VecDeque::new();
+    let mut p0 = Program::new(&instructions, 0);
+    q = p0.run(q);
+    println!("{}", q.iter().last().unwrap());
 
     // part 2
-    let mut p0 = Program::new(&instructions, 0);
     let mut p1 = Program::new(&instructions, 1);
 
-    let mut q = VecDeque::new();
     loop {
-        q = p0.run(q);
         let p0_waiting = q.is_empty();
         q = p1.run(q);
         if p0_waiting && q.is_empty() {
             break;
         }
+        q = p0.run(q);
     }
 
     println!("{}", p1.sent_messages);
