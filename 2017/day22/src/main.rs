520a7539cf99686bdad10c5dab79ea63e1932bba
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-08 18:27:20 +0100

    2017: Visualize day 22

--- 2017/day22/src/main.rs
+++ 2017/day22/src/main.rs
@@ -1,4 +1,16 @@
+//! Part 1 is a variant of [Langton's Ant]. Run this program with the
+//! `visualize` feature enabled to see how the ant starts building a recurrent
+//! "highway" (near the end of the visualization):
+//!
+//!     cargo run --release --features=visualize
+//!
+//! [Langton's Ant]: https://en.wikipedia.org/wiki/Langton%27s_ant
 use std::fs;
+
+#[cfg(feature = "visualize")]
+use screen::WindowedScreen;
+
+const SIZE: usize = 1024;
 
 #[derive(Clone, Copy)]
 enum State {
@@ -6,6 +18,21 @@ enum State {
     Weakened,
     Infected,
     Flagged,
+}
+
+#[cfg(feature = "visualize")]
+fn visualize(grid: &[State], virus: (usize, usize), screen: &mut WindowedScreen) {
+    let mut map = grid
+        .iter()
+        .map(|s| match s {
+            State::Clean => (' ', (0, 0, 0)),
+            State::Weakened => ('⬮', (237, 167, 57)),
+            State::Infected => ('⬮', (208, 58, 32)),
+            State::Flagged => ('⬮', (112, 216, 215)),
+        })
+        .collect::<Vec<_>>();
+    map[virus.1 * SIZE + virus.0] = ('█', (135, 247, 83));
+    screen.update_with_colors(map, virus);
 }
 
 fn main() {
@@ -16,26 +43,33 @@ fn main() {
         .collect::<Vec<_>>();
 
     for part1 in [true, false] {
+        #[cfg(feature = "visualize")]
+        let mut screen =
+            WindowedScreen::new(SIZE, SIZE, if part1 { 400 } else { 800 }, Some((2, 2)));
+
         // a map of 1024 * 1024 should be more than large enough (for my input,
         // even 512x512 worked well)
-        let mut map = vec![State::Clean; 1024 * 1024];
+        let mut map = vec![State::Clean; SIZE * SIZE];
         for (y, row) in grid.iter().enumerate() {
             for (x, cell) in row.iter().enumerate() {
                 if *cell == b'#' {
-                    map[(y + 512) * 1024 + x + 512] = State::Infected;
+                    map[(y + SIZE / 2) * SIZE + x + SIZE / 2] = State::Infected;
                 }
             }
         }
 
         let mut dir = (0, -1);
         let mut virus = (
-            (grid[0].len() / 2 + 512) as usize,
-            (grid.len() / 2 + 512) as usize,
+            (grid[0].len() / 2 + SIZE / 2) as usize,
+            (grid.len() / 2 + SIZE / 2) as usize,
         );
 
+        #[cfg(feature = "visualize")]
+        visualize(&map, virus, &mut screen);
+
         let mut infects = 0;
-        for _ in 0..(if part1 { 10_000 } else { 10_000_000 }) {
-            let s = &mut map[virus.1 * 1024 + virus.0];
+        for _frame in 0..(if part1 { 10_000 } else { 10_000_000 }) {
+            let s = &mut map[virus.1 * SIZE + virus.0];
             match s {
                 State::Clean => {
                     dir = (dir.1, -dir.0);
@@ -61,6 +95,20 @@ fn main() {
             }
             virus.0 = virus.0.checked_add_signed(dir.0).unwrap();
             virus.1 = virus.1.checked_add_signed(dir.1).unwrap();
+
+            #[cfg(feature = "visualize")]
+            // Always visualize the entirety of part 1, but only frames 0-15000
+            // and 9995000-10000000 of part 2. The visualization would be way
+            // too long otherwise.
+            if part1 || !(15000..=9995000).contains(&_frame) {
+                visualize(&map, virus, &mut screen);
+            }
+        }
+
+        #[cfg(feature = "visualize")]
+        {
+            screen.finish();
+            println!();
         }
 
         println!("{infects}");
--- README.md
+++ README.md
@@ -60,21 +60,25 @@ I've created terminal visualizations of some puzzles for fun:
       <a href="2016/day22/src/main.rs">2016 - Day 22: Grid Computing</a>
     </td>
     <td align="center">
-      <a href="2019/day13/aoc_2019_day13.gif"><img src="2019/day13/aoc_2019_day13.gif" height="200px" /></a><br />
-      <a href="2019/day13/src/main.rs">2019 - Day 13: Care Package</a>
+      <a href="2017/day22/aoc_2017_day22.webp"><img src="2017/day22/aoc_2017_day22_mini.webp" height="200px" /></a><br />
+      <a href="2017/day22/src/main.rs">2017 - Day 22: Sporifica Virus</a>
     </td>
   </tr>
   <tr>
+    <td align="center">
+      <a href="2019/day13/aoc_2019_day13.gif"><img src="2019/day13/aoc_2019_day13.gif" height="200px" /></a><br />
+      <a href="2019/day13/src/main.rs">2019 - Day 13: Care Package</a>
+    </td>
     <td align="center">
       <a href="2024/day15/aoc_2024_day15.gif"><img src="2024/day15/aoc_2024_day15.gif" height="200px" /></a><br />
       <a href="2024/day15/src/main.rs">2024 - Day 15: Warehouse Woes</a>
     </td>
+  </tr>
+  <tr>
     <td align="center">
       <a href="2025/day04/aoc_2025_day04.gif"><img src="2025/day04/aoc_2025_day04.gif" height="200px" /></a><br />
       <a href="2025/day04/src/main.rs">2025 - Day 4: Printing Department</a>
     </td>
-  </tr>
-  <tr>
     <td align="center">
       <a href="2025/day12/aoc_2025_day12.gif"><img src="2025/day12/aoc_2025_day12.gif" height="200px" /></a><br />
       <a href="2025/day12/src/main.rs">2025 - Day 12: Christmas Tree Farm</a>
