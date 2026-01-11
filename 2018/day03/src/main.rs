15aebd7a1c4312ba71c3117cdbe72064b511f6b1
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-11 15:32:51 +0100

    2018: Optimize day 3

--- 2018/day03/src/main.rs
+++ 2018/day03/src/main.rs
@@ -1,50 +1,95 @@
-use std::{collections::HashMap, fs};
+use std::{collections::BTreeMap, fs};
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
-    let claims = input
-        .lines()
-        .map(|l| {
-            let (_, rest) = l.split_once(" @ ").unwrap();
-            let (coords, size) = rest.split_once(": ").unwrap();
-            let (x, y) = coords.split_once(',').unwrap();
-            let x = x.parse::<usize>().unwrap();
-            let y = y.parse::<usize>().unwrap();
-            let (w, h) = size.split_once('x').unwrap();
-            let w = w.parse::<usize>().unwrap();
-            let h = h.parse::<usize>().unwrap();
-            (x, y, w, h)
-        })
-        .enumerate()
-        .collect::<Vec<_>>();
-
-    let mut fabric: HashMap<(usize, usize), usize> = HashMap::new();
-
-    // mark used squares
-    for (_, c) in &claims {
-        for x in c.0..(c.0 + c.2) {
-            for y in c.1..(c.1 + c.3) {
-                *fabric.entry((x, y)).or_default() += 1;
-            }
-        }
+    let mut events = Vec::new();
+    let mut rectangles = Vec::new();
+    for l in input.lines() {
+        let mut parts = l.split_ascii_whitespace();
+        let coords = parts.nth(2).unwrap();
+        let size = parts.next().unwrap();
+        let (x, y) = coords.split_once(',').unwrap();
+        let x = x.parse::<i64>().unwrap();
+        let y = y[0..y.len() - 1].parse::<i64>().unwrap();
+        let (w, h) = size.split_once('x').unwrap();
+        let w = w.parse::<i64>().unwrap();
+        let h = h.parse::<i64>().unwrap();
+        events.push((x, y, h));
+        events.push((x + w, y, -h));
+        rectangles.push((x, y, w, h));
     }
+    events.sort_unstable();
 
-    // part 1: count how many squares are used more than once
-    let sum = fabric.values().filter(|&&c| c > 1).count();
-    println!("{}", sum);
+    // part 1 - perform sweep-line algorithm to count number of overlapping grid
+    // cells
+    let mut col: BTreeMap<i64, i64> = BTreeMap::new();
+    let mut last_x = 0;
+    let mut overlapping = 0;
+    let mut total = 0;
+    let mut i = 0;
+    while i < events.len() {
+        // start of a new range of events for column x
+        let x = events[i].0;
 
-    // part 2: find the ID of the one claim that never overlaps
-    for (i, c) in &claims {
-        let mut only_ones = true;
-        for x in c.0..(c.0 + c.2) {
-            for y in c.1..(c.1 + c.3) {
-                if *fabric.get(&(x, y)).unwrap() != 1 {
-                    only_ones = false;
-                    break;
+        // update total
+        total += overlapping * (x - last_x);
+        last_x = x;
+
+        // for each event in the current column ...
+        let mut j = i;
+        while j < events.len() {
+            let (nextx, y, h) = events[j];
+            if nextx != x {
+                break;
+            }
+            // update column
+            if h > 0 {
+                *col.entry(y).or_default() += 1;
+                let end = col.entry(y + h).or_default();
+                *end -= 1;
+                if *end == 0 {
+                    col.remove(&(y + h));
                 }
+            } else {
+                let start = col.entry(y).or_default();
+                *start -= 1;
+                if *start == 0 {
+                    col.remove(&y);
+                }
+                *col.entry(y - h).or_default() += 1;
+            }
+            j += 1;
+        }
+
+        // iterate over column, maintain sum, and update number of overlapping
+        // grid cells
+        overlapping = 0;
+        let mut sum = 0;
+        let mut last_y = 0;
+        for (&y, &v) in col.iter() {
+            if sum > 1 {
+                overlapping += y - last_y;
+            }
+            sum += v;
+            last_y = y;
+        }
+
+        i = j;
+    }
+    println!("{total}");
+
+    // part 2 - check for overlaps
+    for (i, a) in rectangles.iter().enumerate() {
+        let mut good = true;
+        for b in rectangles.iter().skip(i + 1) {
+            if !(b.0 >= a.0 + a.2 || a.0 >= b.0 + b.2 || b.1 >= a.1 + a.3 || a.1 >= b.1 + b.3) {
+                // rectangles are not disjoint
+                good = false;
+                break;
             }
         }
-        if only_ones {
+        if good {
+            // rectangle a does not overlap any other rectangle
             println!("{}", i + 1);
             break;
         }
