3115f1ccdb76a871fd1f1d26f998ef1ecb9fb9d0
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-22 07:41:59 +0100

    2018: Optimize day 12

--- 2018/day12/src/main.rs
+++ 2018/day12/src/main.rs
@@ -1,84 +1,130 @@
-use std::{collections::HashSet, fs};
+use std::fs;
+
+fn step(
+    state: &[u64],
+    pos: i64,
+    len: usize,
+    rules: &[bool; 32],
+    new_state: &mut Vec<u64>,
+) -> (i64, usize) {
+    new_state.clear();
+    new_state.push(0);
+
+    let mut index_last = 0;
+    let mut j = 4;
+
+    let mut i = state[0].trailing_zeros() as usize - 4;
+    let pos = pos + i as i64 - 2;
+    while i < len {
+        let d = i / 64;
+        let m = i % 64;
+        let mut w = ((state[d] >> m) & 0b11111) as usize;
+        if m >= 60 && d + 1 < state.len() {
+            w |= (state[d + 1] as usize & ((1 << (m - 59)) - 1)) << (64 - m);
+        }
+
+        if j % 64 == 0 {
+            new_state.push(0);
+            index_last += 1;
+        }
+        if rules[w] {
+            new_state[index_last] |= 1 << j;
+        }
+
+        j += 1;
+        i += 1;
+    }
+
+    (
+        pos,
+        index_last * 64 + (64 - new_state[index_last].leading_zeros() as usize),
+    )
+}
+
+fn sum(state: &[u64], pos: i64, len: usize) -> i64 {
+    let mut result = 0;
+    for i in state[0].trailing_zeros() as usize..len {
+        if state[i / 64] >> (i % 64) & 1 > 0 {
+            result += i as i64 + pos;
+        }
+    }
+    result
+}
 
 fn main() {
-    for part1 in [true, false] {
-        let input = fs::read_to_string("input.txt").expect("Could not read file");
-        let mut lines = input.lines();
-        let initial_state = lines.next().unwrap();
-        let (_, initial_state) = initial_state.split_once(": ").unwrap();
+    let input = fs::read_to_string("input.txt").expect("Could not read file");
+    let mut lines = input.lines();
+    let initial_state = lines.next().unwrap();
+    let initial_state = &initial_state[15..];
 
-        lines.next();
-        let mut rules = [false; 32];
-        lines.for_each(|l| {
-            let parts = l.split_once(" => ").unwrap();
-            if parts.1 == "#" {
-                let mut p = 0usize;
-                for (i, c) in parts.0.chars().enumerate() {
-                    if c == '#' {
-                        p += 1 << i;
-                    }
-                }
-                rules[p] = true;
-            }
-        });
-
-        let mut min_i = i64::MAX;
-        let mut max_i = i64::MIN;
-        let mut state = HashSet::new();
-        let mut prev = String::new();
-        for (i, c) in initial_state.chars().enumerate() {
-            if c == '#' {
-                state.insert(i as i64);
-                min_i = min_i.min(i as i64);
-                max_i = max_i.max(i as i64);
-            }
+    // Parse initial state into a bit vector. Leave four bits empty at the
+    // beginning, so extracting bits in `step()` is easier.
+    let mut state: Vec<u64> = vec![0];
+    let mut index_last = 0;
+    for (i, b) in initial_state.bytes().enumerate() {
+        if (i + 4) % 64 == 0 {
+            state.push(0);
+            index_last += 1;
         }
-
-        let max_len = if part1 { 20 } else { 50_000_000_000usize };
-        let mut k = 0;
-        while k < max_len {
-            let mut new_state = HashSet::new();
-            let mut new_min_i = i64::MAX;
-            let mut new_max_i = i64::MIN;
-            let mut current = String::new();
-            for i in min_i - 2..=max_i + 2 {
-                let mut p = 0usize;
-                for j in 0..5 {
-                    if state.contains(&(i + j - 2)) {
-                        p += 1 << j;
-                    }
-                }
-                if rules[p] {
-                    new_state.insert(i);
-                    new_min_i = new_min_i.min(i);
-                    new_max_i = new_max_i.max(i);
-                    current.push('#');
-                } else {
-                    current.push('.');
-                }
-            }
-            state = new_state;
-            min_i = new_min_i;
-            max_i = new_max_i;
-
-            if !part1 {
-                if prev == current {
-                    break;
-                }
-                prev = current;
-            }
-
-            k += 1;
+        if b == b'#' {
+            state[index_last] |= 1 << ((i + 4) % 64);
         }
-
-        let sum = if part1 {
-            state.into_iter().sum::<i64>()
-        } else {
-            state
-                .into_iter()
-                .map(|v| v + (max_len - k - 1) as i64)
-                .sum::<i64>()
-        };
-        println!("{}", sum);
     }
+
+    // `pos` represents the ID of the pot at the beginning of the bit vector
+    let mut pos = -4;
+
+    // `len` is the length of the bit vector. Truncate it at the last set bit.
+    let mut len = index_last * 64 + (64 - state[index_last].leading_zeros() as usize);
+
+    // parse rules into a table with all possible 2^5 bit patterns
+    lines.next();
+    let mut rules = [false; 32];
+    for l in lines {
+        let bytes = l.as_bytes();
+        let from = &bytes[0..5];
+        let to = bytes[9];
+        if to == b'#' {
+            let mut p = 0_usize;
+            for (i, &b) in from.iter().enumerate() {
+                if b == b'#' {
+                    p += 1 << i;
+                }
+            }
+            rules[p] = true;
+        }
+    }
+
+    // part 1 - simulate the first 20 steps
+    let mut prev_state = Vec::new();
+    let mut steps = 0_i64;
+    while steps < 20 {
+        (state, prev_state) = (prev_state, state);
+        (pos, len) = step(&prev_state, pos, len, &rules, &mut state);
+        steps += 1;
+    }
+    println!("{}", sum(&state, pos, len));
+
+    // part 2 - only simulate until the pattern repeats
+    let mut prev_pos = 0;
+    while steps < 50_000_000_000 {
+        (state, prev_state) = (prev_state, state);
+        prev_pos = pos;
+        (pos, len) = step(&prev_state, prev_pos, len, &rules, &mut state);
+        steps += 1;
+        if state == prev_state {
+            // pattern has repeated
+            break;
+        }
+    }
+
+    // extrapolate to 50 billion steps
+    println!(
+        "{}",
+        sum(
+            &state,
+            pos + (pos - prev_pos) * (50_000_000_000 - steps),
+            len,
+        )
+    );
 }
