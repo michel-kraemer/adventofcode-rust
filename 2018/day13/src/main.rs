c351c53bf99205bb8f7180098684c8b2cdd1bf7b
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-01-23 18:34:42 +0100

    2018: Optimize day 13

--- 2018/day13/src/main.rs
+++ 2018/day13/src/main.rs
@@ -1,13 +1,12 @@
 use std::{fs, mem};
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+#[derive(PartialEq, Eq, Copy, Clone)]
 enum Turn {
     Left,
     Straight,
     Right,
 }
 
-#[allow(clippy::needless_range_loop)]
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Could not read file");
     let mut grid = input
@@ -15,32 +14,36 @@ fn main() {
         .map(|l| l.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    let mut carts: Vec<(i32, i32, i32, i32, Turn)> = Vec::new();
-    for y in 0..grid.len() {
-        for x in 0..grid[y].len() {
-            let c = grid[y][x];
+    let mut carts: Vec<(i32, i32, i32, i32, usize, Turn)> = Vec::new();
+    let mut cart_positions = vec![vec![usize::MAX; grid[0].len()]; grid.len()];
+    for (y, row) in grid.iter_mut().enumerate() {
+        for (x, c) in row.iter_mut().enumerate() {
             match c {
                 '>' => {
-                    grid[y][x] = '-';
-                    carts.push((y as i32, x as i32, 0, 1, Turn::Left));
+                    *c = '-';
+                    cart_positions[y][x] = carts.len();
+                    carts.push((y as i32, x as i32, 0, 1, carts.len(), Turn::Left));
                 }
                 '<' => {
-                    grid[y][x] = '-';
-                    carts.push((y as i32, x as i32, 0, -1, Turn::Left));
+                    *c = '-';
+                    cart_positions[y][x] = carts.len();
+                    carts.push((y as i32, x as i32, 0, -1, carts.len(), Turn::Left));
                 }
                 'v' => {
-                    grid[y][x] = '|';
-                    carts.push((y as i32, x as i32, 1, 0, Turn::Left));
+                    *c = '|';
+                    cart_positions[y][x] = carts.len();
+                    carts.push((y as i32, x as i32, 1, 0, carts.len(), Turn::Left));
                 }
                 '^' => {
-                    grid[y][x] = '|';
-                    carts.push((y as i32, x as i32, -1, 0, Turn::Left));
+                    *c = '|';
+                    cart_positions[y][x] = carts.len();
+                    carts.push((y as i32, x as i32, -1, 0, carts.len(), Turn::Left));
                 }
                 _ => {}
             }
         }
     }
-    carts.sort();
+    carts.sort_unstable_by_key(|c| (c.0, c.1));
 
     let mut first_crashed = false;
 
@@ -48,6 +51,7 @@ fn main() {
         let mut sci = 0;
         while sci < carts.len() {
             let sc = &mut carts[sci];
+            cart_positions[sc.0 as usize][sc.1 as usize] = usize::MAX;
 
             let c = grid[sc.0 as usize][sc.1 as usize];
             match c {
@@ -67,14 +71,14 @@ fn main() {
                 }
 
                 '+' => {
-                    let ndir = match sc.4 {
+                    let ndir = match sc.5 {
                         Turn::Left => (-sc.3, sc.2, Turn::Straight),
                         Turn::Straight => (sc.2, sc.3, Turn::Right),
                         Turn::Right => (sc.3, -sc.2, Turn::Left),
                     };
                     sc.2 = ndir.0;
                     sc.3 = ndir.1;
-                    sc.4 = ndir.2;
+                    sc.5 = ndir.2;
                 }
 
                 _ => panic!(),
@@ -87,28 +91,26 @@ fn main() {
             let scx = sc.1;
 
             let mut crashed = false;
-            for j in 0..carts.len() {
-                if j == sci {
-                    continue;
+            let other_cart = &mut cart_positions[scy as usize][scx as usize];
+            if *other_cart != usize::MAX {
+                let oci = carts.iter().position(|c| c.4 == *other_cart).unwrap();
+                if oci < sci {
+                    carts.remove(sci);
+                    carts.remove(oci);
+                    sci -= 1;
+                } else {
+                    carts.remove(oci);
+                    carts.remove(sci);
                 }
-                let nc = carts[j];
-                if nc.0 == scy && nc.1 == scx {
-                    if j < sci {
-                        carts.remove(j);
-                        sci -= 1;
-                        carts.remove(sci);
-                    } else {
-                        carts.remove(sci);
-                        carts.remove(j - 1);
-                    }
-                    crashed = true;
-                    break;
-                }
+                crashed = true;
+                *other_cart = usize::MAX;
+            } else {
+                *other_cart = sc.4;
             }
 
             if crashed && !first_crashed {
                 // part 1
-                println!("{},{}", scx, scy);
+                println!("{scx},{scy}");
                 first_crashed = true;
             }
 
@@ -117,7 +119,7 @@ fn main() {
             }
         }
 
-        carts.sort();
+        carts.sort_unstable_by_key(|c| (c.0, c.1));
     }
 
     // part 2
