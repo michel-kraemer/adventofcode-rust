509fd918d756c526282ec57105c1a0ea1f9a4cd9
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-02-02 07:46:56 +0100

    2018: Visualize day 17 (draft)

--- 2018/day17/src/visualize.rs
+++ 2018/day17/src/visualize.rs
@@ -1,6 +1,6 @@
 use screen::{
     WindowedScreen,
-    style::{StyledContent, Stylize, style},
+    style::{Stylize, style},
 };
 
 #[derive(Debug)]
@@ -10,6 +10,7 @@ struct StreamHead {
     volume: f32,
 }
 
+/// Simple pseudo-random generator. See https://en.wikipedia.org/wiki/Xorshift.
 struct Xorshift {
     x32: u32,
 }
@@ -27,49 +28,51 @@ impl Xorshift {
     }
 }
 
-fn should_fill(
-    x: usize,
-    y: usize,
-    grid: &[u8],
-    new_grid: &[StyledContent<char>],
-    width: usize,
-    water: &StyledContent<char>,
-) -> bool {
-    grid[(y + 1) * width + x] == b'#' || new_grid[(y + 1) * width + x] == *water
+fn should_fill(x: usize, y: usize, grid: &[u8], wetness: &[f32], width: usize) -> bool {
+    grid[(y + 1) * width + x] == b'#' || wetness[(y + 1) * width + x] == 1.0
 }
 
 pub fn visualize(grid: &[u8], width: usize, height: usize, start_x: usize) {
-    let mut screen = WindowedScreen::new(width, height, 40, Some((20, 6)));
+    let dried = style('█').with((104, 104, 91).into());
+    let water_33 = style('█').with((197, 177, 134).into());
+    let water_66 = style('█').with((163, 171, 180).into());
+    let water = style('█').with((95, 158, 211).into());
+    let water_darker_33 = style('█').with((171, 144, 97).into());
+    let water_darker_66 = style('█').with((129, 139, 150).into());
+    let water_darker = style('█').with((70, 127, 178).into());
+    let water_lighter_33 = style('█').with((219, 205, 164).into());
+    let water_lighter_66 = style('█').with((196, 200, 200).into());
+    let water_lighter = style('█').with((142, 195, 233).into());
+    let sand = style('█').with((210, 177, 111).into());
+    let sand_darker = style('█').with((190, 142, 67).into());
+    let sand_lighter = style('█').with((228, 205, 145).into());
 
+    let mut screen = WindowedScreen::new(width, height, 40, Some((20, 6)));
     let mut rng = Xorshift::new();
-    let sand_colors = [(190, 142, 67), (228, 205, 145)];
 
     let mut new_grid = vec![style(' '); width * height];
     let mut wetness = vec![0.0; width * height];
+
     for y in 0..height {
         for x in 0..width {
             let c = match grid[y * width + x] {
                 b'#' => style('█').with((130, 100, 45).into()),
                 _ => {
                     let r = rng.next() % 800;
-                    let col = if r > 50 {
-                        (210, 177, 111)
+                    if r > 50 {
+                        sand
+                    } else if r.is_multiple_of(2) {
+                        sand_darker
                     } else {
-                        sand_colors[(r as usize) % sand_colors.len()]
-                    };
-                    style('█').with(col.into()).on((210, 177, 111).into())
+                        sand_lighter
+                    }
                 }
             };
             new_grid[y * width + x] = c;
         }
     }
 
-    let dried = style('█')
-        .with((104, 104, 91).into())
-        .on((128, 128, 75).into());
-    let water_33 = style('█').with((197, 177, 134).into());
-    let water_66 = style('█').with((163, 171, 180).into());
-    let water = style('█').with((70, 127, 178).into());
+    let unmodified_new_grid = new_grid.clone();
 
     let mut last_max_x = 0;
     let mut last_max_y = 0;
@@ -87,23 +90,23 @@ pub fn visualize(grid: &[u8], width: usize, height: usize, start_x: usize) {
                 continue;
             }
 
-            if should_fill(s.x, s.y, grid, &new_grid, width, &water) {
+            if should_fill(s.x, s.y, grid, &wetness, width) {
                 let mut left = s.x;
                 while left > 0
                     && grid[s.y * width + left - 1] != b'#'
-                    && should_fill(left, s.y, grid, &new_grid, width, &water)
+                    && should_fill(left, s.y, grid, &wetness, width)
                 {
                     left -= 1;
                 }
                 let mut right = s.x;
                 while right < width - 1
                     && grid[s.y * width + right + 1] != b'#'
-                    && should_fill(right, s.y, grid, &new_grid, width, &water)
+                    && should_fill(right, s.y, grid, &wetness, width)
                 {
                     right += 1;
                 }
-                if !should_fill(left, s.y, grid, &new_grid, width, &water)
-                    && !should_fill(right, s.y, grid, &new_grid, width, &water)
+                if !should_fill(left, s.y, grid, &wetness, width)
+                    && !should_fill(right, s.y, grid, &wetness, width)
                 {
                     for x in left..=right {
                         new_grid[s.y * width + x] = dried;
@@ -118,13 +121,13 @@ pub fn visualize(grid: &[u8], width: usize, height: usize, start_x: usize) {
                         y: s.y,
                         volume: s.volume / 2.0,
                     });
-                } else if !should_fill(left, s.y, grid, &new_grid, width, &water) {
+                } else if !should_fill(left, s.y, grid, &wetness, width) {
                     for x in left..=right {
                         new_grid[s.y * width + x] = dried;
                     }
                     s.x = left;
                     new_queue.push(s);
-                } else if !should_fill(right, s.y, grid, &new_grid, width, &water) {
+                } else if !should_fill(right, s.y, grid, &wetness, width) {
                     for x in left..=right {
                         new_grid[s.y * width + x] = dried;
                     }
@@ -137,18 +140,33 @@ pub fn visualize(grid: &[u8], width: usize, height: usize, start_x: usize) {
                         wetness[s.y * width + x] = w;
                         max = max.max(w);
                     }
-                    let fill = if max == 1.0 {
-                        Some(water)
-                    } else if max >= 0.66 {
-                        Some(water_66)
-                    } else if max >= 0.33 {
-                        Some(water_33)
-                    } else {
-                        None
-                    };
-                    if let Some(fill) = fill {
+                    if max >= 0.33 {
                         for x in left..=right {
-                            if fill == water || new_grid[s.y * width + x] != dried {
+                            if max == 1.0 || new_grid[s.y * width + x] != dried {
+                                let ug = unmodified_new_grid[s.y * width + x];
+                                let fill = if ug == sand_darker {
+                                    if max == 1.0 {
+                                        water_darker
+                                    } else if max >= 0.66 {
+                                        water_darker_66
+                                    } else {
+                                        water_darker_33
+                                    }
+                                } else if ug == sand_lighter {
+                                    if max == 1.0 {
+                                        water_lighter
+                                    } else if max >= 0.66 {
+                                        water_lighter_66
+                                    } else {
+                                        water_lighter_33
+                                    }
+                                } else if max == 1.0 {
+                                    water
+                                } else if max >= 0.66 {
+                                    water_66
+                                } else {
+                                    water_33
+                                };
                                 new_grid[s.y * width + x] = fill;
                             }
                         }
@@ -199,14 +217,15 @@ pub fn visualize(grid: &[u8], width: usize, height: usize, start_x: usize) {
         if queue.is_empty() {
             break;
         }
-        let max_s = queue.last().unwrap();
 
+        let max_s = queue.last().unwrap();
         screen.update_with_style(new_grid.clone(), (max_s.x, max_s.y));
         last_max_x = max_s.x;
         last_max_y = max_s.y;
     }
 
     screen.update_with_style(new_grid.clone(), (last_max_x, last_max_y));
+
     drop(screen);
 
     println!();
