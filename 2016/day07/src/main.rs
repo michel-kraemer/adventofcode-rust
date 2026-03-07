61590d7b80f8b7d94e5655feaedac661d833c228
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2026-03-07 07:43:18 +0100

    2016: Optimize day 7

--- 2016/day07/src/main.rs
+++ 2016/day07/src/main.rs
@@ -5,8 +5,8 @@ fn has_abba(s: &str) -> bool {
         return false;
     }
     let s = s.as_bytes();
-    for w in s.windows(4) {
-        if w[0] != w[1] && w[0] == w[3] && w[1] == w[2] {
+    for &[a, b, c, d] in s.array_windows() {
+        if a != b && a == d && b == c {
             return true;
         }
     }
@@ -19,9 +19,9 @@ fn get_abas(s: &str) -> Vec<(u8, u8)> {
         return result;
     }
     let sb = s.as_bytes();
-    for w in sb.windows(3) {
-        if w[0] != w[1] && w[0] == w[2] {
-            result.push((w[0], w[1]));
+    for &[a, b, c] in sb.array_windows() {
+        if a != b && a == c {
+            result.push((a, b));
         }
     }
     result
@@ -33,23 +33,23 @@ fn main() {
     let mut result_part1 = 0;
     let mut result_part2 = 0;
     for l in input.lines() {
-        let a = l.split(&['[', ']']).collect::<Vec<_>>();
+        let s = l.split(&['[', ']']).collect::<Vec<_>>();
 
         let mut abas = Vec::new();
         let mut tls_good = false;
         let mut tls_bad = false;
-        for p in a.iter().step_by(2) {
+        for p in s.iter().step_by(2) {
             tls_good |= has_abba(p);
             abas.extend(get_abas(p));
         }
 
         let mut ssl_good = false;
-        for p in a.iter().skip(1).step_by(2) {
+        for p in s.iter().skip(1).step_by(2) {
             tls_bad |= has_abba(p);
             if !ssl_good {
-                'outer: for w in p.as_bytes().windows(3) {
-                    for a in &abas {
-                        if w[0] == a.1 && w[1] == a.0 && w[2] == a.1 {
+                'outer: for &[a, b, c] in p.as_bytes().array_windows() {
+                    for d in &abas {
+                        if a == d.1 && b == d.0 && c == d.1 {
                             ssl_good = true;
                             break 'outer;
                         }
